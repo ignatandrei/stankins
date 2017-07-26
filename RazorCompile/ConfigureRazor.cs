9e1242be901a5d9160df61408fe71c82329c0de5
ignatandrei <ignatandrei@yahoo.com>
2017-07-26 05:33:08 +0300

    passing the tests

---
+++ RazorCompile/ConfigureRazor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.Extensions.PlatformAbstractions;
+using RazorLight;
+using RazorLight.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorCompile
+{
+
+    public class ConfigureRazor: IRazorRenderer
+    {
+        public ConfigureRazor()
+        {
+                        
+        }
+
+        public async Task<string> RenderToString<TModel>(string contentView, TModel model)
+        {
+            var engine = EngineFactory.CreatePhysical(AppContext.BaseDirectory);
+            try
+            {
+                return engine.ParseString<TModel>(contentView, model);
+            }
+            catch (Exception ex)
+            {
+                //TODO: log
+                throw;
+            }
+
+        }
+    }
+}
