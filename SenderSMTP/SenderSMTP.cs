769009d7fedcb15fca960b214326e5ecb55bf55f
Bogdan SAHLEAN <bsahlean@hotmail.com>
2017-08-07 01:45:20 +0300

    Added SenderSMTP + associated unit test

---
+++ SenderSMTP/SenderSMTP.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Threading.Tasks;
+using StankinsInterfaces;
+using System.Text;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MailKit.Security;
+
+namespace SenderSMTP
+{
+    public class SenderToSMTP : ISend
+    {
+        public string To { get; private set; }
+        public string Cc { get; set; }
+        public string Bcc { get; private set; }
+        public string Subject { get; private set; }
+        public string Body { get; private set; }
+        public bool IsBodyHtml { get; private set; }
+
+        public string SmtpServer { get; private set; }
+        public int SmtpPort { get; private set; }
+        public bool EnableSsl { get; private set; }
+        public bool RequiresAuthentication { get; private set; }
+        public string User { get; private set; }
+        public string Password { get; private set; }
+
+        public IRow[] valuesToBeSent { set; get; }
+
+        public SenderToSMTP(string to, string cc, string bcc, string subject, string body, bool isBodyHtml, string smtpServer, int smtpPort = 25, bool enableSsl = false, bool requiresAuthentication = true, string user = "", string password = "")
+        {
+            this.To = to;
+            this.Cc = cc;
+            this.Bcc = bcc;
+            this.Subject = subject;
+            this.Body = body;
+            this.IsBodyHtml = IsBodyHtml;
+            this.SmtpServer = smtpServer;
+            this.SmtpPort = smtpPort;
+            this.EnableSsl = enableSsl;
+            this.RequiresAuthentication = requiresAuthentication;
+            if (!this.RequiresAuthentication && (!string.IsNullOrEmpty(user) || !string.IsNullOrEmpty(password)))
+            {
+                throw new Exception("If RequiresAuthentication is false then user and password should be empty");
+            }
+            if (this.RequiresAuthentication && (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password)))
+            {
+                //throw new Exception("If RequiresAuthentication is true then user and password are mandatory");
+                throw new NotImplementedException();
+            }
+            this.User = user;
+            this.Password = password;
+        }
+
+        public async Task Send()
+        {
+            //Generate email body
+            const string csvDelimiter = ",";
+            bool hasHeader = false;
+            StringBuilder data = new StringBuilder();
+
+            foreach (var row in valuesToBeSent)
+            {
+                bool firstValue = true;
+                if(!hasHeader)
+                {
+                    foreach (var value in row.Values)
+                    {
+                        data.Append((firstValue ? string.Empty : csvDelimiter)).Append(value.Key);
+                        firstValue = false;
+                    }
+                    data.AppendLine();
+                    hasHeader = true;
+                }
+
+                firstValue = true;
+                foreach (var value in row.Values)
+                {
+                    data.Append((firstValue ? string.Empty : csvDelimiter)).Append(value.Value);
+                    firstValue = false;
+                }
+                data.AppendLine();
+            }
+
+            this.Body = this.Body + data;
+
+            //Send email
+            //Using MailKit 
+            //https://www.stevejgordon.co.uk/how-to-send-emails-in-asp-net-core-1-0
+            //https://www.joeaudette.com/blog/2016/05/08/sending-smtp-email-on-aspnet-core-with-mailkit
+            //TODO: In .Net Core 2.0 replace MailKit with MailMessage
+            var message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress("me@localhost"));
+            foreach(var emailAddress in this.To.Split(';'))
+            {
+                if (string.IsNullOrEmpty(emailAddress))
+                {
+                    break;
+                }
+                message.To.Add(new MailboxAddress(emailAddress));
+            }
+            foreach (var emailAddress in this.Cc.Split(';'))
+            {
+                if(string.IsNullOrEmpty(emailAddress))
+                {
+                    break;
+                }
+                message.Cc.Add(new MailboxAddress(emailAddress));
+            }
+            foreach (var emailAddress in this.Bcc.Split(';'))
+            {
+                if (string.IsNullOrEmpty(emailAddress))
+                {
+                    break;
+                }
+                message.Bcc.Add(new MailboxAddress(emailAddress));
+            }
+            message.Subject = this.Subject;
+            message.Body = new TextPart((this.IsBodyHtml ? "html" : "plain")) { Text = this.Body };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(this.SmtpServer, this.SmtpPort, (this.EnableSsl ? SecureSocketOptions.Auto : SecureSocketOptions.None) ).ConfigureAwait(false);
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                // Note: only needed if the SMTP server requires authentication
+                if (this.RequiresAuthentication)
+                {
+                    await client.AuthenticateAsync(this.User,this.Password).ConfigureAwait(false);
+                }
+                await client.SendAsync(message).ConfigureAwait(false);
+                await client.DisconnectAsync(true).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ StankinsTests/TestSenderSMTP.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Moq;
+using StankinsInterfaces;
+using SenderSMTP;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MailKit.Security;
+using MailKit;
+using MailKit.Net.Pop3;
+
+[TestClass]
+public class TestSenderSMTP
+{
+    //public TestContext TestContext { get; set; }
+
+    [TestMethod]
+    [TestCategory("ExternalProgramsToBeRun")]
+    public async Task TestSendEmail()
+    {
+        string to = "bogdan@localhost";
+        string smtpServer = "127.0.0.1";
+        int smtpPort = 25;
+        string subject = "SenderSMTP.test" + DateTime.Now.ToString();
+        bool requiresAuthentication = false;
+        string user = string.Empty;
+        string password = string.Empty;
+
+        #region arrange
+        //Prepare source data: 2 rows {ID, FirstName, LastName}
+        var m = new Mock<IRow>();
+        var rows = new List<IRow>();
+        int nrRows = 2;
+
+        for (int i = 0; i < nrRows; i++)
+        {
+            var row = new Mock<IRow>();
+            row.SetupProperty
+            (
+                obj => obj.Values,
+                new Dictionary<string, object>()
+                {
+                    ["PersonID"] = i,
+                    ["FirstName"] = "John " + i,
+                    ["LastName"] = "Doe " + i
+                }
+            );
+
+            rows.Add(row.Object);
+        }
+
+        var sender = new SenderToSMTP(to, string.Empty, string.Empty, subject, string.Empty, false, smtpServer, smtpPort, false, requiresAuthentication, user, password);
+        sender.valuesToBeSent = rows.ToArray();
+        #endregion
+
+        #region act
+        await sender.Send(); //smtp4dev should be enabled an should listen on 127.0.0.1:25
+        #endregion
+
+        #region assert
+        //If it's doesn't throw an exception it's ok
+        #endregion
+
+    }
+}
