0b8608ba49e05a5f3ad7f957f8a716dd7f9f2221
ignatandrei <ignatandrei@yahoo.com>
2017-08-17 08:24:39 +0300

    test .net 20

---
+++ TestNETStandard20/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+
+namespace TestNETStandard20
+{
+    public class Class1
+    {
+        public DataTable MyProperty { get; set; }
+        public Class1()
+        {
+            MyProperty = new DataTable("ASda");
+            MyProperty.Columns.Add("ASD", typeof(int));
+        }
+    }
+}
