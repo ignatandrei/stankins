9c10c86cd4761fac26a5170edb7369f3e12b4327
Bogdan SAHLEAN <bsahlean@gmail.com>
2017-08-22 01:59:49 +0300

    [1] FilterComparable Serialization + Des [2] Unit test for serialization

---
+++ StankinsTests/TestFilterComparableSerializationDeserialization.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Transformers;
+
+namespace StankinsTests
+{
+    [TestClass]
+    public class TestFilterComparableSerializationDeserialization
+    {
+        [TestMethod]
+        public async Task TestFilterComparableSerialization()
+        {
+            #region arrange
+            string filterAsString = "System.Int32 ColumnA = 3000";
+            #endregion
+
+            #region act
+            var filterAsObject = (FilterComparableEqual)((TypeConverter)new FilterComparable()).ConvertFrom(filterAsString);
+            #endregion
+
+            #region assert
+            Assert.IsNotNull(filterAsObject);
+            Assert.AreEqual("System.Int32", filterAsObject.ComparableType.ToString());
+            Assert.AreEqual("ColumnA", filterAsObject.FieldName);
+            Assert.AreEqual(CompareValues.Equal, filterAsObject.HowToCompareValues);
+            Assert.AreEqual(3000, filterAsObject.Value);
+            #endregion
+        }
+    }
+}
--- Transformers/FilterComparable.cs
+++ Transformers/FilterComparable.cs
@@ -1,13 +1,15 @@
 ﻿using StankinsInterfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Transformers
 {
-    public class FilterComparable : ITransform
-        
+    public class FilterComparable : TypeConverter, ITransform
+
     {
         public IRow[] valuesRead { get; set ; }
         public IRow[] valuesTransformed { get ; set ; }
@@ -23,6 +25,11 @@ namespace Transformers
             FieldName = fieldName;
             HowToCompareValues = compareValues;
         }
+
+        public FilterComparable()
+        {
+        }
+
         public async Task Run()
         {
             IComparable v = (IComparable)Value;
@@ -52,7 +59,6 @@ namespace Transformers
                         add = (res <= 0);
                         break;
                     default:
-                        //TODO: add implementation for LessOrEqual or GreaterOrEqual
                         throw new ArgumentException("This is not implemented :" + HowToCompareValues);
                 }
                 if (!add)
@@ -64,6 +70,60 @@ namespace Transformers
             valuesTransformed = returnValues.ToArray();
 
             await Task.CompletedTask;
+        }
+
+        // Deserialization (ex. "int ColumnA >= 3000"). The separator is a single space/' '. 0 = type, 1 = fieldName, 2 = operator, 3 = value.
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if(value is string)
+            {
+                string[] tokens = ((string)value).Split(' ');
+                Type filterDataType = Type.GetType(tokens[0]);
+                string filterFieldName = tokens[1];
+                string filterOperator = tokens[2];
+                object filterValue = TypeDescriptor.GetConverter(filterDataType).ConvertFrom(context, culture, tokens[3]);
+
+                FilterComparable filter = null;
+                switch(filterOperator)
+                {
+                    case "=":
+                        filter = new FilterComparableEqual(filterDataType, filterValue, filterFieldName);
+                        break;
+                    case "<":
+                        filter = new FilterComparableLess(filterDataType, filterValue, filterFieldName);
+                        break;
+                    case "<=":
+                        filter = new FilterComparableLessOrEqual(filterDataType, filterValue, filterFieldName);
+                        break;
+                    case ">":
+                        filter = new FilterComparableGreat(filterDataType, filterValue, filterFieldName);
+                        break;
+                    case ">=":
+                        filter = new FilterComparableGreaterOrEqual(filterDataType, filterValue, filterFieldName);
+                        break;
+                    default:
+                        throw new ArgumentException("This is not implemented :" + filterOperator);
+                }
+                return filter;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+        // Serialization
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            return destinationType == typeof(string) || base.CanConvertTo(context, destinationType);
+        }
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if( destinationType == typeof(string) )
+            {
+                return $"{value.GetType().ToString()} {this.FieldName} {this.HowToCompareValues} {(string)value}";
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
         }
     }
 }
