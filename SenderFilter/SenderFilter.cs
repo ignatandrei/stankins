baf914df78d04432761ae92cabf803fec98099c2
Bogdan SAHLEAN <bsahlean@gmail.com>
2017-08-22 13:18:31 +0300

    [1] Added SenderWithFilter: filter + ISend (it works with SimpleJob) [2] Added one unit test + one intergration test

---
+++ SenderFilter/SenderFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using StankinsInterfaces;
+using Transformers;
+using System.ComponentModel;
+
+namespace SenderFilter
+{
+    public class SenderWithFilter : ISend
+    {
+        public string Filter { get; set; } //Syntax: "DataType FieldName Operator ConstValue"  (ex. "System.Int32 ElapsedTime >= 3000")
+        public ISend Sender { get; set; }
+        public IRow[] valuesToBeSent { set; get; }
+
+        public SenderWithFilter() { }
+
+        public SenderWithFilter(string filter, ISend sender) {
+            this.Filter = filter;
+            this.Sender = sender;
+        }
+
+        public async Task Send()
+        {
+            //Deserialize Filter into TransformerFilter_
+            var filterAsObject = (FilterComparable)((TypeConverter)new FilterComparable()).ConvertFrom(this.Filter);
+            filterAsObject.valuesRead = this.valuesToBeSent;
+            //Apply filter
+            await filterAsObject.Run();
+            this.valuesToBeSent = filterAsObject.valuesTransformed;
+
+            //Send data to real sender
+            this.Sender.valuesToBeSent = this.valuesToBeSent;
+            await this.Sender.Send();
+        }
+    }
+}
