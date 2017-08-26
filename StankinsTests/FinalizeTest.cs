608e98b1814d24faf2d0812f2d07bd09d0b37570
ignatandrei <ignatandrei@yahoo.com>
2017-08-26 21:45:41 +0300

    added test with duration

--- StankinsTests/FinalizeTest.cs
+++ StankinsTests/FinalizeTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace StankinsTests
@@ -20,6 +21,12 @@ namespace StankinsTests
         public static void FinishAllTest()
         {
             Console.WriteLine("finished test!");
+            var data = Logging.StartLogging.cd.Select(it => new { Key=it.Key(), it.duration }).ToArray();
+            var time= data.GroupBy(it => it.Key).Select(it => new { it.Key, MilliSeconds = it.Sum(t => t.duration.TotalMilliseconds)/it.Count() }).OrderByDescending(s=>s.MilliSeconds).Take(10).ToArray();
+            foreach(var item in time)
+            {
+                Console.WriteLine($"!!!!!Method {item.Key} duration seconds {item.MilliSeconds}");
+            }
             string x = "";
         }
     }
