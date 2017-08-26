1cc5ec252181ea0afc6d248fa70e76207ffccb41
ignatandrei <ignatandrei@yahoo.com>
2017-08-27 00:41:30 +0300

    added number runs

--- StankinsTests/FinalizeTest.cs
+++ StankinsTests/FinalizeTest.cs
@@ -24,15 +24,16 @@ namespace StankinsTests
             var data = Logging.StartLogging.cd.Select(it => new { Key=it.Key(), it.duration }).ToArray();
             var g = data.GroupBy(it => it.Key).ToArray();
             var meanTime= g.Select(it => new { it.Key, MilliSeconds = it.Sum(t => t.duration.TotalMilliseconds)/it.Count() }).OrderByDescending(s=>s.MilliSeconds).Take(10).ToArray();
+            Console.WriteLine("!mean time");
             foreach(var item in meanTime)
             {
                 Console.WriteLine($"!!!!!Method {item.Key} mean duration seconds {item.MilliSeconds/1000}");
             }
-
-            var totalTime= g.Select(it => new { it.Key, MilliSeconds = it.Sum(t => t.duration.TotalMilliseconds) }).OrderByDescending(s => s.MilliSeconds).Take(10).ToArray();
+            Console.WriteLine("!max time");
+            var totalTime= g.Select(it => new { it.Key, MilliSeconds = it.Sum(t => t.duration.TotalMilliseconds) ,nr=it.Count()}).OrderByDescending(s => s.MilliSeconds).Take(10).ToArray();
             foreach (var item in totalTime)
             {
-                Console.WriteLine($"!!!!!Method {item.Key} total duration seconds {item.MilliSeconds / 1000}");
+                Console.WriteLine($"!!!!!Method {item.Key} total duration seconds {item.MilliSeconds / 1000} number runs: {item.nr}");
             }
             string x = "";
         }
