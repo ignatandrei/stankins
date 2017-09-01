cf38f700c456ad32e04d3037617992678518e6b9
ignatandrei <ignatandrei@yahoo.com>
2017-09-01 22:08:24 +0300

    added oledb provider

---
+++ ReceiverOLEDB/ReceiverOleDBDatabase.cs
@@ -0,0 +1,28 @@
+﻿using ReceiverDB;
+using StankinsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiverOLEDB
+{
+    public class ReceiverOleDBDatabaseInt: ReceiverOleDBDatabase<int>
+    {
+        public ReceiverOleDBDatabaseInt(DBTableData<int, OleDbConnection> dtd):base(dtd)
+        {
+
+        }
+    }
+    public class ReceiverOleDBDatabase<T> : ReceiverTable<T, OleDbConnection>
+         where T : IComparable<T>         
+    {
+        public ReceiverOleDBDatabase(DBTableData<T, OleDbConnection> dtd):base(dtd)
+        {
+            
+        }
+    }
+}
