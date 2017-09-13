2d3690ed9b5af836db4d65f815e8107af10e9664
Bogdan SAHLEAN <bsahlean@gmail.com>
2017-09-13 07:59:50 +0300

    Added ReceiverFromAzureIoTHub (.Net Core)

---
+++ ReceiverAzureIoTHub/LastReceivedMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceiverAzureIoTHub
+{
+    public struct LastReceivedMessage
+    {
+        public DateTime EnqueuedTimeUtc;
+        public long EnqueuedOffset;
+    }
+}
