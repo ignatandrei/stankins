eeaf9ccac1bd49091b9e317cccdc8e855dc17cb3
ignatandrei <ignatandrei@yahoo.com>
2017-09-15 02:37:35 +0300

    add usings

--- ReceiverFile/ReceiverFileFromStorage.cs
+++ ReceiverFile/ReceiverFileFromStorage.cs
@@ -5,7 +5,7 @@ using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.Logging;
 namespace ReceiverFile
 {
     public class ReceiverFileFromStorageBinary : IReceive
--- StanskinsImplementation/SyncReceiverMultiple.cs
+++ StanskinsImplementation/SyncReceiverMultiple.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.Logging;
 namespace StanskinsImplementation
 {
     public class SyncReceiverMultiple : IReceive
