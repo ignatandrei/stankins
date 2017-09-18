ff958a3a9a779cfc2e8882abacefe7604dd5c8b7
ignatandrei <ignatandrei@yahoo.com>
2017-09-18 06:42:48 +0300

    added tranfdorm from file list to file lines

---
+++ StankinsTests/TestTransformerLine.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReceiverFileSystem;
+using Shouldly;
+using StanskinsImplementation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transformers;
+
+namespace StankinsTests
+{
+    [TestClass]
+    public class TestTransformerLine
+    {
+        [TestMethod]
+
+        public async Task TestTransformerFileToLines()
+        {
+            #region arrange
+            var dir = AppContext.BaseDirectory;
+
+            var folderSql = Path.Combine(dir, "SqlToExecute");
+            var receiverFolder = new ReceiverFolderHierarchical(folderSql, "*.txt");
+
+            #endregion
+            #region act
+            var transformer = new TransformerFileToLines();
+            var j = new SimpleJob();
+            j.Receivers.Add(0, receiverFolder);
+            j.FiltersAndTransformers.Add(0, transformer);
+            await j.Execute();
+            #endregion
+            #region assert
+            transformer.valuesTransformed.ShouldNotBeNull();
+            var files = transformer.valuesTransformed.GroupBy(it => it.Values["FullName"]).ToArray();
+            files.Length.ShouldBeGreaterThan(1);
+            var file1Len = files.First().Count();
+            var file2Len = files.Last().Count();
+            file1Len.ShouldBeGreaterThan(0);
+            file2Len.ShouldBeGreaterThan(0);
+            (file1Len + file2Len).ShouldBeGreaterThan(2);
+            #endregion
+        }
+    }
+}
---
+++ Transformers/TransformerFileToLines.cs
@@ -0,0 +1,56 @@
+﻿using StankinsInterfaces;
+using StanskinsImplementation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transformers
+{
+    public class TransformerFileToLines : ITransform
+    {
+        public IRow[] valuesRead { get; set ; }
+        public IRow[] valuesTransformed { get; set ; }
+        public string Name { get ; set ; }
+
+        public async Task Run()
+        {
+            List<IRow> r = new List<IRow>();
+            foreach(var item in valuesRead)
+            {
+                if (!item.Values.ContainsKey("FullName"))
+                {
+                    //TODO: log
+                    continue;
+                }
+                var file = item.Values["FullName"]?.ToString();
+                if (!File.Exists(file))
+                {
+                    //TODO: log
+                    continue;
+                }
+                var lines = File.ReadAllLines(file);
+                int i = 0;
+                var length = lines.Length;
+                var padding = length.ToString().Length;
+                foreach(var line in lines)
+                {
+                    #region padding
+                    //todo: use padleft
+                    string id = (++i).ToString();
+                    while (id.ToString().Length < padding)
+                    {
+                        id = "0" + id;
+                    }
+                    #endregion
+                    var rr = new RowRead();
+                    rr.Values.Add($"line{id}", line);
+                    rr.Values.Add("FullName", file);
+                    r.Add(rr);
+                }
+            }
+            valuesTransformed = r.ToArray();
+        }
+    }
+}
