91930de8669bd20cf0113c6458893ae57e99c742
ignatandrei <ignatandrei@yahoo.com>
2017-09-25 01:59:54 +0300

    pushing other demos

--- StankinsDemos/makeDemo.bat
+++ StankinsDemos/makeDemo.bat
@@ -1,3 +1,6 @@
 rem xcopy Demo* StankinsD /I /F /R /E /Y 
 xcopy Demo1JobFolders StankinsDemo\Demo1JobFolders  /I /F /R /E /Y 
 xcopy Demo2SimpleJobView StankinsDemo\Demo2SimpleJobView  /I /F /R /E /Y 
+xcopy Demo3ExecuteSql StankinsDemo\Demo3ExecuteSql  /I /F /R /E /Y 
+xcopy Demo4DocumentSqlServer StankinsDemo\Demo4DocumentSqlServer  /I /F /R /E /Y 
+xcopy Demo5PBX StankinsDemo\Demo5PBX  /I /F /R /E /Y 
