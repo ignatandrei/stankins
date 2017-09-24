76e947e5c6fa2082ed24ef7013d0231a61fd59fd
georgesacasanu95 <pseudonim2000@gmail.com>
2017-09-24 09:29:34 +0300

    MySql

---
+++ ReceiverDBMySQL/ReceiverTableMySQL.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+using ReceiverDB;
+using System;
+
+namespace ReceiverDBMySQL
+{
+    public abstract class ReceiverTableMySQL<T> : ReceiverTable<T, MySqlConnection>
+    where T : IComparable<T>
+    {
+        public ReceiverTableMySQL(DBTableData<T, MySqlConnection> dtd) : base(dtd)
+        {
+        }
+    }
+}
