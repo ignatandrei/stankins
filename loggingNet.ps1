07dc84e5ff4d81665fc75bec093a2cf4d4e444d1
ignatandrei <ignatandrei@yahoo.com>
2017-10-04 07:20:35 +0300

    now logging replacing for .net 4

--- loggingNet.ps1
+++ loggingNet.ps1
@@ -2,6 +2,6 @@ param([Parameter(Mandatory=$true)][string]$sourceDirectory)
 
 Write-Host "start modifying " $sourceDirectory
 
-Copy-Item $sourceDirectory\LoggingNET  $sourceDirectory\Logging -Container -Force -recurse
-
+Copy-Item $sourceDirectory\LoggingNET\*.*  $sourceDirectory\Logging\ -Container -Force 
+Move-Item $sourceDirectory\Logging\LoggingNet.csproj $sourceDirectory\Logging\Logging.csproj -Force 
 Write-Host "end modifying " $sourceDirectory
