37619bc38194a45a2377481c13e71f0b6d1f43eb
ignatandrei <ignatandrei@yahoo.com>
2017-10-12 05:31:43 +0300

    html decode

---
+++ StankinsTests/Test_TransformerHtmlEncode.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Shouldly;
+using StankinsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TransformerHtmlUrl;
+
+namespace StankinsTests
+{
+    [TestClass]
+    public class Test_TransformerHtmlDecode
+    {
+        [TestMethod]
+        public async Task TransformerHtmlDecodeSimpleText()
+        {
+#region arrange
+            var str = "&lt;block type=&#x27;SimpleJob&#x27;&gt;&lt;/block&gt;";
+            var rows = new List<IRow>();
+            var rowAndrei = new Mock<IRow>();
+
+            rowAndrei.SetupProperty(it => it.Values,
+                new Dictionary<string, object>()
+                {
+                    
+                    ["str"] = str                    
+                }
+            );
+
+            rows.Add(rowAndrei.Object);
+            #endregion
+            #region act
+            var t = new TransformerHtmlDecode();
+            t.valuesRead = rows.ToArray();
+            await t.Run();
+            #endregion
+            #region assert
+            t.valuesTransformed.ShouldNotBeNull();
+            t.valuesTransformed.Length.ShouldBe(1);
+            var val = t.valuesTransformed[0].Values;
+            val.ShouldNotBeNull();
+            val.ShouldContainKeyAndValue("str", "<block type='SimpleJob'></block>");
+
+
+            
+            #endregion
+        }
+    }
+}
---
+++ TransformerHtmlUrl/TransformerHtmlDecode.cs
@@ -0,0 +1,28 @@
+﻿using StankinsInterfaces;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TransformerHtmlUrl
+{
+    public class TransformerHtmlDecode : ITransform
+    {
+        public IRow[] valuesRead { get ; set ; }
+        public IRow[] valuesTransformed { get ; set ; }
+        public string Name { get ; set ; }
+
+        public async Task Run()
+        {
+            valuesTransformed = valuesRead;
+            foreach(var vt in valuesTransformed)
+            {
+                var keys = vt.Values.Keys.ToArray();
+                foreach (var key in keys)
+                {
+                    vt.Values[key] = WebUtility.HtmlDecode(vt.Values[key]?.ToString());
+                }
+            }
+        }
+    }
+}
