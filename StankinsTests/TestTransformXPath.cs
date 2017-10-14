ec0b721d57b9064b78c7293a8024fbe2a8c0ec04
Bogdan SAHLEAN <bsahlean+github@gmail.com>
2017-10-14 13:18:36 +0300

    TransformXPath

---
+++ StankinsTests/TestTransformXPath.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using StankinsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Transformers;
+
+namespace StankinsTests
+{
+    [TestClass]
+    public class TestTransformXPath
+    {
+        [TestMethod]
+        public async Task TestTransformXPathSimple()
+        {
+            #region arange
+            var rows = new List<IRow>();
+            var row0 = new Mock<IRow>();
+            row0.SetupProperty(it => it.Values,
+                new Dictionary<string, object>()
+                {
+                    ["ID"] = 0,
+                    ["XmlEventData"] = "" 
+                }
+            );
+            rows.Add(row0.Object);
+
+            var row1 = new Mock<IRow>();
+            row1.SetupProperty(it => it.Values,
+                new Dictionary<string, object>()
+                {
+                    ["ID"] = 1,
+                    ["XmlEventData"] = "       "
+                }
+            );
+            rows.Add(row1.Object);
+
+            var row2 = new Mock<IRow>();
+            row2.SetupProperty(it => it.Values,
+                new Dictionary<string, object>()
+                {
+                    ["ID"] = 2,
+                    ["XmlEventData"] = null
+                }
+            );
+            rows.Add(row2.Object);
+
+            var row3 = new Mock<IRow>();
+            row3.SetupProperty(it => it.Values,
+                new Dictionary<string, object>()
+                {
+                    ["ID"] = 2,
+                    ["XmlEventData"] = 
+@"<event name=""wait_info"" package=""sqlos"" timestamp=""2017-10-04T21:17:15.426Z"">
+    <data name=""wait_type"">
+        <value>3</value>
+        <text>LCK_M_S</text>
+    </data>
+        <data name=""duration"">
+        <value>7303</value>
+    </data>
+        <action name=""database_name"" package=""sqlserver"">
+    <value>Test01</value>
+    </action>
+        <action name=""client_app_name"" package=""sqlserver"">
+    <value/>
+    </action>
+</event>"
+                }
+            );
+            rows.Add(row3.Object);
+            #endregion
+
+            #region act
+            var transform = new TransformXPath(@"wait_type=XmlEventData(event/data[@name = ""wait_type""]/text/text())[1];duration=XmlEventData(event/data[@name = ""duration""]/value/text())[1];db=XmlEventData(event/action[@name = ""db""]/value/text())[1]");
+            transform.valuesRead = rows.ToArray();
+            await transform.Run();
+            #endregion
+
+            #region assert
+            int i = 0;
+            foreach (var item in transform.valuesTransformed)
+            {
+                Assert.IsTrue(item.Values.ContainsKey("wait_type"));
+                Assert.IsTrue(item.Values.ContainsKey("duration"));
+                Assert.IsTrue(item.Values.ContainsKey("db"));
+                if(i < 3)
+                {
+                    Assert.IsNull(item.Values["wait_type"]);
+                    Assert.IsNull(item.Values["duration"]);
+                    Assert.IsNull(item.Values["db"]);
+                }
+                else if( i == 3)
+                {
+                    Assert.AreEqual("LCK_M_S", item.Values["wait_type"]);
+                    Assert.AreEqual("7303", item.Values["duration"]);
+                    Assert.IsNull(item.Values["db"]);
+                }
+                i++;
+            }
+            #endregion
+        }
+    }
+}
---
+++ Transformers/TransformXPath.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StankinsInterfaces;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using System.IO;
+
+namespace Transformers
+{
+    public class TransformXPath : ITransform
+    {
+        public TransformXPath(string keyExpressionPairs)
+        {
+
+            this.KeyExpressionPairs = keyExpressionPairs;
+            this.Name = $"applying XPath expression(s) {keyExpressionPairs}";
+        }
+        public IRow[] valuesRead { get; set; }
+        public IRow[] valuesTransformed { get; set; }
+        public string Name { get; set; }
+        public string KeyExpressionPairs { get; set; } //KeyTargetColumn=KeySourceColumn(event/data[@name = "wait_type"]/text/text())[1];Key02=Expr02;Key03=Expr03
+        protected Dictionary<string, XPathExpression> expressionsToBeEvaluated;
+
+        protected void Init()
+        {
+            expressionsToBeEvaluated = new Dictionary<string, XPathExpression>();
+            string[] tokens = this.KeyExpressionPairs.Split(';');
+            foreach(var token in tokens)
+            {
+                string[] keyExpressionPair = token.Split("=(".ToCharArray(), 3);
+                expressionsToBeEvaluated.Add(keyExpressionPair[0], new XPathExpression() { SourceColumn = keyExpressionPair[1], Expression = '(' + keyExpressionPair[2] });
+            }
+        }
+
+        public async Task Run()
+        {
+            Init();
+
+            foreach (var row in valuesRead)
+            {
+                foreach(var exp in expressionsToBeEvaluated)
+                {
+                    if (row.Values.ContainsKey(exp.Value.SourceColumn))
+                    {
+                        string sourceXmlValue = (string)row.Values[exp.Value.SourceColumn];
+                        if(!string.IsNullOrWhiteSpace(sourceXmlValue))
+                        {
+                            using (TextReader tr = new StringReader(sourceXmlValue))
+                            {
+                                XPathDocument document = new XPathDocument(tr);
+                                XPathNavigator nav = document.CreateNavigator();
+                                try
+                                {
+                                    row.Values[exp.Key] = nav.SelectSingleNode(exp.Value.Expression).Value;
+                                }
+                                catch (Exception)
+                                {
+
+                                    row.Values[exp.Key] = null;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            row.Values[exp.Key] = null;
+                        }
+                    }
+                    else
+                    {
+                        row.Values[exp.Key] = null;
+                    }
+                }
+            }
+
+            valuesTransformed = valuesRead;
+        }
+
+        protected struct XPathExpression
+        {
+            public string SourceColumn;
+            public string Expression;
+        }
+    }
+}
