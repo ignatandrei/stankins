88b9b6609f7250067379a74342bd9aaa2efe27ed
ignatandrei <ignatandrei@yahoo.com>
2017-10-23 06:14:40 +0300

    added razor to generate

---
+++ HtmlGenerator/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace HtmlGenerator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            //Process.Start("notepad.exe");
+        }
+    }
+}
