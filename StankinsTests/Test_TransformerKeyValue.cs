2e980dafec2d203f38d20cec71e2359159b39d21
ignatandrei <ignatandrei@yahoo.com>
2017-11-12 09:38:05 +0200

    transform into key value

---
+++ StankinsTests/Test_TransformerKeyValue.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Shouldly;
+using StankinsInterfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TransformerHtmlUrl;
+using Transformers;
+
+namespace StankinsTests
+{
+    [TestClass]
+    public class Test_TransformerKeyValue
+    {
+        [TestMethod]
+        public async Task TransformerKeyValue()
+        {
+            #region arrange
+            var rows = new List<IRow>();
+            var m = new Mock<IRow>();
+            
+            int nrRows = 2;
+
+            for (int i = 0; i < nrRows; i++)
+            {
+                var row = new Mock<IRow>();
+                row.SetupProperty
+                (
+                    obj => obj.Values,
+                    new Dictionary<string, object>()
+                    {
+                        ["PersonID"] = i,
+                        ["FirstName"] =(i%2==0)?"": "John " + i,
+                        ["LastName"] = (i % 2 == 1) ? "":"Doe " + i
+                    }
+                );
+
+                rows.Add(row.Object);
+            }
+
+            
+            #endregion
+            #region act
+            var t = new TransformKeyValue();
+            t.valuesRead = rows.ToArray();
+            await t.Run();
+            #endregion
+            #region assert
+            t.valuesTransformed.ShouldNotBeNull();
+            t.valuesTransformed.Length.ShouldBe(2);
+            var val = t.valuesTransformed[0].Values;
+            val.ShouldNotBeNull();
+            val.ShouldContainKeyAndValue("key",0);
+            val.ShouldContainKeyAndValue("value", "Doe 0");
+
+            val = t.valuesTransformed[1].Values;
+            val.ShouldNotBeNull();
+            val.ShouldContainKeyAndValue("key", 1);
+            val.ShouldContainKeyAndValue("value", "John 1");
+
+
+
+
+            #endregion
+        }
+    }
+}
---
+++ Transformers/TransformKeyValue.cs
@@ -0,0 +1,56 @@
+﻿using StankinsInterfaces;
+using StanskinsImplementation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transformers
+{
+    public class TransformKeyValue : ITransform
+    {
+        public IRow[] valuesRead { get; set; }
+        public IRow[] valuesTransformed { get; set; }
+        public string Name { get; set; }
+        
+        
+        public TransformKeyValue()
+        {
+            Name = $"transform into key value";
+            
+        }
+
+        public async Task Run()
+        {
+            int nrValues = valuesRead?.Length ?? 0;
+            if (nrValues == 0)
+                return ;
+            valuesTransformed = new RowRead[nrValues];
+            for (int i = 0; i < nrValues; i++)
+            {
+                var rr = new RowRead();
+                foreach(var item in valuesRead[i].Values)
+                {
+                    if (item.Value == null)
+                        continue;
+                    if (string.IsNullOrWhiteSpace(item.Value.ToString()))
+                        continue;
+                    if (!rr.Values.ContainsKey("key")) {
+                        rr.Values["key"] = item.Value;
+                        continue;
+                    }
+                    if (rr.Values.ContainsKey("value"))
+                    {
+                        throw new ArgumentException("more than 1 value for " + item.Key);
+                    }
+
+                    rr.Values.Add("value",item.Value );
+                    
+                }
+                valuesTransformed[i] = rr;
+            }
+
+
+        }
+    }
+}
