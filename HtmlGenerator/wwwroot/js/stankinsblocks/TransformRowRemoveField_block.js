d6f2814695494ed9e66d8ef7236c44233b591ce3
ignatandrei <ignatandrei@yahoo.com>
2017-11-24 05:54:39 +0200

    adding remove field

--- HtmlGenerator/wwwroot/js/stankinsblocks/TransformRowRemoveField_block.js
+++ HtmlGenerator/wwwroot/js/stankinsblocks/TransformRowRemoveField_block.js
@@ -2,18 +2,16 @@
         Blockly.Blocks['TransformRowRemoveField'] = {
         init: function() {
         this.appendDummyInput()
-        .appendField("Remove Fields");
-		
-        this.appendValueInput('valName') 
-        .setCheck('String')
-        .appendField('Name:')
-        .appendField(new Blockly.FieldTextInput('Remove fields'), 'fldName');
+        .appendField("TransformRowRemoveField");
         
         this.appendValueInput('valNameFields') 
         .setCheck('Array')
-		.setAlign(Blockly.ALIGN_RIGHT)
-        .appendField('name of fields (append list)')
-        //.appendField(new Blockly.FieldTextInput(''), 'fldNameFields');
+        .appendField('NameFields:')
+        .appendField(new Blockly.FieldTextInput(''), 'fldNameFields');
+        this.appendValueInput('valName') 
+        .setCheck('String')
+        .appendField('Name:')
+        .appendField(new Blockly.FieldTextInput(''), 'fldName');
         
         this.setTooltip("TransformRowRemoveField");
         this.setHelpUrl("");
--- HtmlGenerator/wwwroot/js/stankinsblocks/TransformRowRemoveField_code.js
+++ HtmlGenerator/wwwroot/js/stankinsblocks/TransformRowRemoveField_code.js
@@ -2,17 +2,18 @@
         Blockly.JavaScript['TransformRowRemoveField'] = function(block) {
   var text_NameFields = block.getFieldValue('fldNameFields');
   var value_NameFields = Blockly.JavaScript.valueToCode(block, 'valNameFields', Blockly.JavaScript.ORDER_ATOMIC);
-  
 var realValue_NameFields =value_NameFields || "'" + text_NameFields + "'";
   
   var text_Name = block.getFieldValue('fldName');
   var value_Name = Blockly.JavaScript.valueToCode(block, 'valName', Blockly.JavaScript.ORDER_ATOMIC);
 var realValue_Name =value_Name || "'" + text_Name + "'";
   
+  
+  //debugger;
   var code ='{';
         code+="NameFields:"+ realValue_NameFields+",";
-        code+="Name:"+ realValue_Name+",";
-        code+="$type: 'Transformers.TransformRowRemoveField, Transformers'"; ;
+        code+="Name:"+ realValue_Name+",";  
+code+="$type: 'Transformers.TransformRowRemoveField, Transformers'"; ;
 code +='}\n';
   return  [code, Blockly.JavaScript.ORDER_NONE];
 };;
