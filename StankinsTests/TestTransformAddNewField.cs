5519672919971d39a3aa26c74565ce7429b272be
ignatandrei <ignatandrei@yahoo.com>
2017-12-31 19:31:55 +0200

    add column formatter

--- StankinsTests/TestTransformAddNewField.cs
+++ StankinsTests/TestTransformAddNewField.cs
@@ -9,9 +9,48 @@ using Transformers;
 using Shouldly;
 namespace StankinsTests
 {
+
     [TestClass]
     public class TestTransformAddNewField
     {
+        [TestMethod]
+        public async Task TestTransformAddFieldFormatter()
+        {
+            #region arange
+            var rows = new List<IRow>();
+            int nrRows = 10;
+            for (int i = 0; i < nrRows; i++)
+            {
+                var row = new Mock<IRow>();
+
+                row.SetupProperty(it => it.Values,
+                    new Dictionary<string, object>()
+                    {
+                        ["ID"] = i,
+                        ["FirstName"] = "John" + i
+                    }
+                );
+
+                rows.Add(row.Object);
+            }
+            #endregion
+
+            #region act
+            var transform = new TransformAddFieldFormatter("Out", "{ID} is {FirstName}");
+            
+            transform.valuesRead = rows.ToArray();
+            await transform.Run();
+            #endregion
+
+            #region assert
+            for (int i = 0; i < nrRows; i++)
+            {
+                var item = transform.valuesTransformed[i];
+                item.Values.ShouldContainKeyAndValue("Out", string.Format("{0} is {1}",rows[i].Values["ID"],rows[i].Values["FirstName"]));
+            }
+
+            #endregion
+        }
         [TestMethod]
         public async Task TestTransformAddNewFieldSimple()
         {
---
+++ Transformers/TransformAddColumnFormatter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Transformers
+{
+    public class TransformAddFieldFormatter : TransformRow
+    {
+        public TransformAddFieldFormatter(string nameFieldOutput, string stringFormatExpression):base("")
+        {
+            NameFieldOutput = nameFieldOutput;
+            StringFormatExpression = stringFormatExpression;
+        }
+
+        public string NameFieldOutput { get; set; }
+        public string StringFormatExpression { get; set; }
+        protected override string ModifyExpression(string theExpression)
+        {
+            string s=FormatWith(StringFormatExpression);
+            s = $"Values.Add(\"{NameFieldOutput}\"," +"\""+ s + "\"" + "); Values";
+            return s;
+        }
+        //copied after https://haacked.com/archive/2009/01/14/named-formats-redux.aspx/
+        public static string FormatWith(string format)
+        {
+            if (format == null)
+                throw new ArgumentNullException("format");
+
+            List<object> values = new List<object>();
+            string rewrittenFormat = Regex.Replace(format,
+              @"(?<start>\{)+(?<property>[\w\.\[\]]+)(?<format>:[^}]+)?(?<end>\})+",
+              delegate (Match m)
+              {
+                  Group startGroup = m.Groups["start"];
+                  Group propertyGroup = m.Groups["property"];
+                  Group formatGroup = m.Groups["format"];
+                  Group endGroup = m.Groups["end"];
+
+                  //      values.Add((propertyGroup.Value == "0")
+                  //? source
+                  //: Eval(source, propertyGroup.Value));
+                  values.Add(Eval(propertyGroup.Value));
+                  int openings = startGroup.Captures.Count;
+                  int closings = endGroup.Captures.Count;
+
+                  return openings > closings || openings % 2 == 0
+             ? m.Value
+             : new string('{', openings) + (values.Count - 1)
+               + formatGroup.Value
+               + new string('}', closings);
+              },
+              RegexOptions.Compiled
+              | RegexOptions.CultureInvariant
+              | RegexOptions.IgnoreCase);
+
+            return string.Format(rewrittenFormat,values.ToArray());
+        }
+
+        private static string Eval(string expression)
+        {
+            return "\"+"+ $"Values[\"{expression}\"]?.ToString()" + "+\"";
+            
+        }
+    }
+}
