9b3a4e249fe137964e0bb6134d3bb9673bb75889
ignatandrei <ignatandrei@yahoo.com>
2018-11-25 09:19:56 +0200

    ping

---
+++ stankinsv2/solution/StankinsV2/Stankins.Alive/AliveStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Stankins.Interfaces;
+using StankinsCommon;
+using StankinsObjects;
+
+namespace Stankins.Alive
+{
+    public abstract class AliveStatus : BaseObject, IReceive
+    {
+        public AliveStatus(CtorDictionary dataNeeded) : base(dataNeeded)
+        {
+
+        }
+        public override async Task<IMetadata> TryLoadMetadata()
+        {
+            var m = new MetadataTable();
+            m.Tables.Add(new Table()
+            {
+                Name = this.Name
+            });
+            var idTable = m.Tables.First(it => it.Name == this.Name).Id;
+            m.AddColumn("Process", idTable);
+            m.AddColumn("Arguments", idTable);
+            m.AddColumn("To", idTable);
+            m.AddColumn("Result", idTable);
+            m.AddColumn("DetailedResult", idTable);
+            return m;
+        }
+    }
+}
---
+++ stankinsv2/solution/StankinsV2/Stankins.Alive/ReceiverPing.cs
@@ -0,0 +1,20 @@
+﻿using Stankins.Interfaces;
+using StankinsCommon;
+using StankinsObjects;
+using System;
+using System.Threading.Tasks;
+
+namespace Stankins.Alive
+{
+    public class ReceiverPing : AliveStatus, IReceive
+    {
+        public ReceiverPing(CtorDictionary dataNeeded) : base(dataNeeded)
+        {
+        }
+
+        public override Task<IDataToSent> TransformData(IDataToSent receiveData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
