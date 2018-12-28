befe4609a8023e1c54bacdd1d08f0ccc63cd1585
ignatandrei <ignatandrei@yahoo.com>
2018-12-28 15:40:25 +0200

    build accord robocopy

--- stankinsv2/solution/StankinsV2/StankinsAliveAngular/build.bat
+++ stankinsv2/solution/StankinsV2/StankinsAliveAngular/build.bat
@@ -1,3 +1,3 @@
 call ng build --prod --build-optimizer
 
-robocopy dist\StankinsAliveAngular ..\StankinsStatusWeb\wwwroot  /MIR /XD
+(robocopy dist\StankinsAliveAngular ..\StankinsStatusWeb\wwwroot  /MIR /XD) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
