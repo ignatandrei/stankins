f6f8462f48cbad226e86ea29eb989f1876d46bf2
ignatandrei <ignatandrei@yahoo.com>
2019-01-02 09:13:49 +0200

    build library

--- stankinsv2/solution/StankinsV2/StankinsAliveAngular/build.bat
+++ stankinsv2/solution/StankinsV2/StankinsAliveAngular/build.bat
@@ -1,4 +1,5 @@
 copy npm-shrinkwrap.json src/npm-shrinkwrap.json /Y
+call ng build versions-netcore-angular
 call ng build --prod --build-optimizer
 
 (robocopy dist\StankinsAliveAngular ..\StankinsStatusWeb\wwwroot  /MIR /XD) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
