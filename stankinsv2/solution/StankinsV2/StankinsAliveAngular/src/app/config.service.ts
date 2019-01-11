2cf4f7067a08ee8101aeaa76bc9ff2923d5bed85
ignatandrei <ignatandrei@yahoo.com>
2019-01-11 09:17:16 +0200

    what monitor generic

--- stankinsv2/solution/StankinsV2/StankinsAliveAngular/src/app/config.service.ts
+++ stankinsv2/solution/StankinsV2/StankinsAliveAngular/src/app/config.service.ts
@@ -52,10 +52,45 @@ export class Configuration {
         return '!!' + typeAlive;
     }
   }
+  public static TransformToRow(execs: Array<Executor>): [string[], RowConfiguration[]] {
+
+    const types = execs.map(it => it.Data.Type).filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+    const ret: RowConfiguration[] = [];
+    // window.alert(types);
+    execs.forEach(it => {
+
+        let foundType = false;
+        const type = it.Data.Type;
+        ret.forEach( r => {
+          const exist = (r[type] != null) ; // (JSON.stringify(r[type]).length > 0);
+          if (!exist) {
+            r[type] = it;
+            foundType = true;
+            return;
+          }
+        });
+        if (!foundType) {
+          const r = new RowConfiguration(types);
+          r[type] = it;
+          ret.push(r);
+        }
+    });
+    // const r = new RowConfiguration(types);
+    // ret.push(r);
+    // window.alert(ret.length );
+    // ret.forEach( r => {
+    //   window.alert(JSON.stringify(r));
+    // });
+    return [types, ret];
+
+  }
 }
 export class Executor {
   public Data: Data;
   public CustomData: CustomData;
+
 }
 export  class Data {
   Type: string;
--- stankinsv2/solution/StankinsV2/StankinsAliveAngular/src/app/what-monitor/what-monitor.component.ts
+++ stankinsv2/solution/StankinsV2/StankinsAliveAngular/src/app/what-monitor/what-monitor.component.ts
@@ -17,45 +17,11 @@ export class WhatMonitorComponent implements OnInit {
       it => {
         // window.alert(it.UserName);
         // window.alert(JSON.stringify(it.ExecutorsDynamic));
-        this.dataSource = this.TransformToRow(it.ExecutorsDynamic);
+        [this.columnsToDisplay, this.dataSource ] =  Configuration.TransformToRow(it.ExecutorsDynamic);
       }
     );
   }
-  public TransformToRow(execs: Array<Executor>): RowConfiguration[] {
 
-    const types = execs.map(it => it.Data.Type).filter((value, index, self) => {
-      return self.indexOf(value) === index;
-    });
-    this.columnsToDisplay = types;
-    const ret: RowConfiguration[] = [];
-    // window.alert(types);
-    execs.forEach(it => {
-
-        let foundType = false;
-        const type = it.Data.Type;
-        ret.forEach( r => {
-          const exist = (r[type] != null) ; // (JSON.stringify(r[type]).length > 0);
-          if (!exist) {
-            r[type] = it;
-            foundType = true;
-            return;
-          }
-        });
-        if (!foundType) {
-          const r = new RowConfiguration(types);
-          r[type] = it;
-          ret.push(r);
-        }
-    });
-    // const r = new RowConfiguration(types);
-    // ret.push(r);
-    // window.alert(ret.length );
-    // ret.forEach( r => {
-    //   window.alert(JSON.stringify(r));
-    // });
-    return ret;
-
-  }
   public friendlyName(fullName: string): string {
     return Configuration.friendlyName(fullName);
   }
