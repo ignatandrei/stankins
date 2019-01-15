cabfb3a3cd5ec983c03d555dc0cd3ea33591d2c9
ignatandrei <ignatandrei@yahoo.com>
2019-01-15 09:22:03 +0200

    more tests

---
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestReceiverProcess.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Stankins.Alive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StankinsTestXUnit
+{
+    [Trait("ReceiverProcess", "")]
+    [Trait("AfterPublish", "0")]
+    public class TestReceiverProcess
+    {
+        [Fact]
+        public async Task TestPingLocalHost()
+        {
+
+            #region arrange
+            var r = new ReceiverProcessAlive("ping.exe", "localhost -n 1");
+            #endregion
+            #region act
+            var dt = await r.TransformData(null);
+            #endregion
+            #region assert
+            dt.Should().NotBeNull();
+            dt.DataToBeSentFurther.Should().NotBeNull();
+            dt.DataToBeSentFurther.Count.Should().Be(1);
+            dt.DataToBeSentFurther.First().Value.Should().NotBeNull();
+            dt.DataToBeSentFurther.First().Value.Rows.Count.Should().Be(6);
+            var row = dt.DataToBeSentFurther.First().Value.Rows[0];
+            row["IsSuccess"].Should().Be(true);
+            #endregion
+        }
+    }
+}
---
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestReceiverWeb.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Stankins.Alive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StankinsTestXUnit
+{
+    [Trait("ReceiverWeb", "")]
+    [Trait("AfterPublish", "0")]
+    [Trait("ExternalDependency","1")]
+    public class TestReceiverWeb
+    {
+        [Fact]
+        public async Task TestAzureDeployWebSite()
+        {
+            #region arrange
+            var r = new ReceiverWeb("https://azurestankins.azurewebsites.net/api/utils/ping");
+            #endregion
+            #region act
+            var dt = await r.TransformData(null);
+            #endregion
+            #region assert
+            dt.Should().NotBeNull();
+            dt.DataToBeSentFurther.Should().NotBeNull();
+            dt.DataToBeSentFurther.Count.Should().Be(1);
+            dt.DataToBeSentFurther.First().Value.Should().NotBeNull();
+            dt.DataToBeSentFurther.First().Value.Rows.Count.Should().Be(1);
+            var row = dt.DataToBeSentFurther.First().Value.Rows[0];
+            row["IsSuccess"].Should().Be(true);
+            #endregion
+        }
+    }
+}
