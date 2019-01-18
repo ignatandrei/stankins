e6dd1406e12cd6f5b5e8719d04c0b298cc1fdc76
ignatandrei <ignatandrei@yahoo.com>
2019-01-18 21:06:34 +0200

    more test

---
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/FilterRemoveTableTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Stankins.File;
+using Stankins.Interfaces;
+using StankinsObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xbehave;
+using Xunit;
+
+namespace StankinsTestXUnit
+{
+    [Trait("FilterRemoveTableTest", "")]
+    [Trait("AfterPublish", "0")]
+
+    public class FilterRemoveTableTest
+    {
+        [Scenario]
+        [Example("Car, Year{NewLine}Ford, 2000{NewLine}Rolls Royce, 2003{NewLine}Mercedes, 2003")]
+        public void TestSimpleCSV(string fileContents)
+        {
+            IReceive receiver = null;
+            IDataToSent data = null;
+            var nl = Environment.NewLine;
+            fileContents = fileContents.Replace("{NewLine}", nl);
+            $"When I create the receiver csv for the content {fileContents}".x(() => receiver = new ReceiverCSVText(fileContents));
+            $"And I read the data".x(async () => data = await receiver.TransformData(null));
+            $"Then should be a data".x(() => data.Should().NotBeNull());
+            $"With a table".x(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().Be(1);
+            });
+
+            $"And when I filter".x(async () => data = await new FilterRemoveTable(data.Metadata.Tables[0].Name).TransformData(data));
+            $"Then should be a data".x(() => data.Should().NotBeNull());
+            $"With 0 table".x(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().Be(0);
+            });
+            
+
+        }
+
+    }
+}
