8c10f6212c4f9e1326b6ed0f15b39d9ef6a5da54
ignatandrei <ignatandrei@yahoo.com>
2019-01-22 10:15:06 +0200

    TransformTrimTest

---
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TransformTrimTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Stankins.File;
+using Stankins.Interfaces;
+using StankinsObjects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xbehave;
+using Xunit;
+using static System.Environment;
+namespace StankinsTestXUnit
+{
+    [Trait("TransformTrim", "")]
+    [Trait("AfterPublish", "0")]
+    public class TransformTrimTest
+    {
+        [Scenario]
+        [Example("Car,Year{NewLine} Ford ,2000"," Ford ")]
+        public void TestSimpleCSV(string fileContents,string nameWithSpaces)
+        {
+            IReceive receiver = null;
+            IDataToSent data=null;
+            var nl = Environment.NewLine;
+            fileContents = fileContents.Replace("{NewLine}", nl);
+            $"When I create the receiver csv for the content {fileContents}".w(() => receiver = new ReceiverCSVText(fileContents));
+            $"And I read the data".w(async () =>data= await receiver.TransformData(null));
+            $"Then should be a data".w(() => data.Should().NotBeNull());
+            $"With a table".w(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().Be(1);
+            });
+            $"The first row should have spaces {nameWithSpaces}".w(() => data.DataToBeSentFurther[0].Rows[0]["Car"].Should().Be(nameWithSpaces));
+            $"and applying {nameof(TransformTrim)}".w(async () => data = await new TransformTrim().TransformData(data));
+            $"The first row should have no spaces {nameWithSpaces.Trim()}".w(() => data.DataToBeSentFurther[0].Rows[0]["Car"].Should().Be(nameWithSpaces.Trim()));
+
+
+
+        }
+    }
+}
