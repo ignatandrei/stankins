2c78f8c39c5f43e73e22b24005aced5b1a94ec2c
ignatandrei <ignatandrei@yahoo.com>
2019-01-26 00:11:51 +0200

    TestReceiverHtmlList

---
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestReceiverHtmlSelector - Copy.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Stankins.File;
+using Stankins.HTML;
+using Stankins.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xbehave;
+using Xunit;
+using static System.Environment;
+namespace StankinsTestXUnit
+{
+    [Trait("ReceiverHtmlList", "")]
+    [Trait("ExternalDependency", "0")]
+    public class TestReceiverHtmlList
+    {
+        [Scenario]
+        [Example(@"Assets\bookmarks_11_17_17.html",4)]
+        public void TestSimpleCSV(string filepath,int numberTables)
+        {
+
+            IDataToSent data=null;
+            
+            $"receiving the file {filepath} ".w(async () =>
+            {
+                data= await new ReceiverHtmlList(filepath, Encoding.UTF8).TransformData(null);
+            });
+            $"Then should be a data".w(() => data.Should().NotBeNull());
+            $"With {numberTables} tables".w(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().Be(4);
+            });
+
+
+        } 
+    }
+}
