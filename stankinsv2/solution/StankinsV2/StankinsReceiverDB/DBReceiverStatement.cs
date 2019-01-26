6756df6865a2c05b2f402819ee8f5e596a59d1db
ignatandrei <ignatandrei@yahoo.com>
2019-01-26 12:08:25 +0200

    added sql start

---
+++ stankinsv2/solution/StankinsV2/StankinsReceiverDB/DBReceiverStatement.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using Stankins.Interfaces;
+using StankinsCommon;
+using StankinsObjects;
+
+namespace StankinsReceiverDB
+{
+    public class DBReceiverStatement : DatabaseReceiver
+    {
+        protected readonly string stmtSql;
+        public DBReceiverStatement(CtorDictionary dict) : base(dict)
+        {
+            stmtSql = GetMyDataOrThrow<string>(nameof(stmtSql));
+        }
+        public DBReceiverStatement(string connectionString, string connectionType, string sql) : base(connectionString,connectionType)
+        {
+            this.stmtSql = sql;
+        }
+        public override async Task<IDataToSent> TransformData(IDataToSent receiveData)
+        {
+
+            if (receiveData == null)
+            {
+                receiveData = new DataToSentTable();
+
+            }
+            using (var cn = NewConnection())
+            {
+                await cn.OpenAsync();
+                using(var cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = stmtSql;
+                    cmd.CommandType = CommandType.Text;
+                    using(var ir=await cmd.ExecuteReaderAsync())
+                    {
+                        var dt = new DataTable();
+                        dt.Load(ir);
+                        var id = receiveData.AddNewTable(dt);
+                        receiveData.Metadata.AddTable(dt, id);
+                       
+
+                    }
+                }
+            }
+            return receiveData;
+        }
+
+        public override Task<IMetadata> TryLoadMetadata()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ stankinsv2/solution/StankinsV2/StankinsReceiverDB/DatabaseReceiver.cs
@@ -0,0 +1,34 @@
+﻿using Stankins.Interfaces;
+using StankinsCommon;
+using StankinsObjects;
+using System;
+using System.Data.Common;
+
+namespace StankinsReceiverDB
+{
+    public abstract class DatabaseReceiver: BaseObject, IReceive
+    {
+        protected readonly string connectionString;
+        protected readonly string connectionType;
+
+        
+        public DatabaseReceiver(CtorDictionary dict) : base(dict)
+        {
+            connectionString = GetMyDataOrThrow<string>(nameof(connectionString));
+            connectionType = GetMyDataOrThrow<string>(nameof(connectionType));
+        }
+        public DatabaseReceiver(string connectionString, string connectionType) : this(new CtorDictionary()
+        {
+            {nameof(connectionString),connectionString },
+            {nameof(connectionType),connectionType }
+        })
+        {
+
+        }
+        protected virtual DbConnection NewConnection()
+        {
+            return Activator.CreateInstance(Type.GetType(connectionType)) as DbConnection;
+        }
+
+    }
+}
