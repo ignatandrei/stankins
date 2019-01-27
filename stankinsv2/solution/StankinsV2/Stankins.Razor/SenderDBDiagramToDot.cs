54275ee2ebb7008d421b3349ce61477c66745aff
ignatandrei <ignatandrei@yahoo.com>
2019-01-28 01:14:59 +0200

    exporting diagram to text documentation

--- stankinsv2/solution/StankinsV2/Stankins.Razor/SenderDBDiagramToDot.cs
+++ stankinsv2/solution/StankinsV2/Stankins.Razor/SenderDBDiagramToDot.cs
@@ -10,40 +10,39 @@ using System.Threading.Tasks;
 
 namespace Stankins.Razor
 {
-    public class SenderDBDiagramToDot : BaseObject, ISenderToOutput
+    public abstract class SenderDBDiagram : BaseObject, ISenderToOutput
     {
-
         public string InputContents { get; set; }
         public KeyValuePair<string, string>[] OutputContents { get; set; }
 
-        public SenderDBDiagramToDot(string inputContents) : this(new CtorDictionary() {
+        public SenderDBDiagram(string inputContents) : this(new CtorDictionary() {
             { nameof(inputContents), inputContents}
-            
+
             }
           )
         {
 
         }
-        public SenderDBDiagramToDot(CtorDictionary dataNeeded) : base(dataNeeded)
+        public SenderDBDiagram(CtorDictionary dataNeeded) : base(dataNeeded)
         {
-           
-            this.InputContents = base.GetMyDataOrDefault<string>(nameof(InputContents),null);
+
+            this.InputContents = base.GetMyDataOrDefault<string>(nameof(InputContents), null);
             if (string.IsNullOrWhiteSpace(this.InputContents))
-                this.InputContents = File.ReadAllText("erdotviz.cshtml");
+                this.InputContents =DefaultText();
 
 
 
         }
-
+        public abstract string DefaultText();
         public override async Task<IDataToSent> TransformData(IDataToSent receiveData)
         {
             var engine = new RazorLightEngineBuilder()
            .UseMemoryCachingProvider()
            .Build();
             var key = Guid.NewGuid().ToString();
-            var found= await engine.CompileRenderAsync<IDataToSent>(key,InputContents,receiveData);
-            
-            
+            var found = await engine.CompileRenderAsync<IDataToSent>(key, InputContents, receiveData);
+
+
             OutputContents = new KeyValuePair<string, string>[] { new KeyValuePair<string, string>(key, found) };
 
 
@@ -54,6 +53,39 @@ namespace Stankins.Razor
         public override Task<IMetadata> TryLoadMetadata()
         {
             throw new NotImplementedException();
+        }
+
+    }
+    public class SenderDBDiagramToDot : SenderDBDiagram, ISenderToOutput
+    {
+
+        public SenderDBDiagramToDot(string inputContents) : base(inputContents)
+        {
+
+        }
+        public SenderDBDiagramToDot(CtorDictionary dataNeeded) : base(dataNeeded)
+        {
+           
+        }
+
+        public override string DefaultText()
+        {
+            return File.ReadAllText("erdotviz.cshtml");
+        }
+    }
+    public class SenderDBDiagramHTMLDocument : SenderDBDiagram, ISenderToOutput
+    {
+        public SenderDBDiagramHTMLDocument(string inputContents) : base(inputContents)
+        {
+        }
+
+        public SenderDBDiagramHTMLDocument(CtorDictionary dataNeeded) : base(dataNeeded)
+        {
+        }
+
+        public override string DefaultText()
+        {
+            return File.ReadAllText("database.cshtml");
         }
     }
 }
--- stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestExportER.cs
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestExportER.cs
@@ -15,11 +15,12 @@ using Xunit;
 
 namespace StankinsTestXUnit
 {
-    [Trait("SenderToDot", "")]
+   
     [Trait("ExternalDependency","SqlServer")]
     public class TestExportER
     {
         [Scenario]
+        [Trait("SenderToDot", "")]
         [Example("Server=(local);Database=msdb;User Id=SA;Password = <YourStrong!Passw0rd>;")]
         public void SenderToDot(string connectionString)
         {
@@ -48,6 +49,43 @@ namespace StankinsTestXUnit
                 sender.OutputContents.Should().NotBeNull();
                 sender.OutputContents.Length.Should().Be(1);
                 //File.WriteAllText(@"C:\Users\Surface1\Desktop\viz.html", sender.OutputContents.First().Value);
+                //Process.Start("notepad.exe","a.txt");
+
+            });
+
+
+
+        }
+        [Scenario]
+        [Trait("SenderDBDiagramHTMLDocument", "")]
+        [Example("Server=(local);Database=msdb;User Id=SA;Password = <YourStrong!Passw0rd>;")]
+        public void SenderToHTML(string connectionString)
+        {
+            IReceive status = null;
+            ISenderToOutput sender = null;
+            IDataToSent data = null;
+            $"Assume Sql Server instance {connectionString} exists , if not see docker folder".w(() => {
+
+            });
+            $"When I create the ReceiverDBServer ".w(() => status = new ReceiveMetadataFromDatabaseSql(connectionString));
+            $"and receive data".w(async () =>
+            {
+                data = await status.TransformData(null);
+            });
+            $"the data should have a tables, columns, relations".w(() =>
+            {
+                data.DataToBeSentFurther.Count.Should().Be(3);
+            });
+
+            $"and now export to SenderToDot".w(async () => {
+                sender = new SenderDBDiagramHTMLDocument("");
+                data = await sender.TransformData(data);
+            });
+            $"should be some content".w(() =>
+            {
+                sender.OutputContents.Should().NotBeNull();
+                sender.OutputContents.Length.Should().Be(1);
+                File.WriteAllText(@"C:\Users\Surface1\Desktop\viz.html", sender.OutputContents.First().Value);
                 //Process.Start("notepad.exe","a.txt");
 
             });
