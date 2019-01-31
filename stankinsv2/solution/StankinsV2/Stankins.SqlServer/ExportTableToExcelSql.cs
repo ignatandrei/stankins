7a6273f2c0a2f70afbc429455305136264337262
ignatandrei <ignatandrei@yahoo.com>
2019-01-31 20:57:41 +0200

    sender excel

---
+++ stankinsv2/solution/StankinsV2/Stankins.SqlServer/ExportTableToExcelSql.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using Stankins.Interfaces;
+using Stankins.Office;
+using Stankins.Razor;
+using StankinsCommon;
+using StankinsObjects;
+
+namespace Stankins.SqlServer
+{
+    public class ExportTableToExcelSql  :BaseObjectInSerial<ReceiveQueryFromDatabaseSql, SenderExcel> , IReceive,ISender
+    {
+        public ExportTableToExcelSql(CtorDictionary dataNeeded) : base(dataNeeded)
+        {
+
+        }
+        //sql injection?
+        public ExportTableToExcelSql(string connectionString, string nameTable,string fileName) : this(new CtorDictionary()
+        {
+            {"sql","select * from "+ nameTable },
+            {nameof(fileName),Path.GetFileName(fileName)},
+            {nameof(connectionString),connectionString },
+            {"folderToSave", Path.GetDirectoryName(fileName)},
+            {"connectionType",typeof(SqlConnection).FullName },         
+            {"addKey",false }
+
+        })
+        {
+
+        }
+    }
+}
--- stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestQuerySql.cs
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestQuerySql.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.IO;
+using FluentAssertions;
 using Stankins.Interfaces;
 using Stankins.SqlServer;
 using Xbehave;
@@ -32,6 +34,37 @@ namespace StankinsTestXUnit
 
 
             $"should be {nrRows} rows".w(() => { data.DataToBeSentFurther[0].Rows.Count.Should().Be(nrRows); });
+
+
+
+        }
+
+        [Scenario]
+        [Trait("ExportTableToExcelSql", "")]
+        [Example("Server=(local);Database=msdb;User Id=SA;Password = <YourStrong!Passw0rd>;", "sys.tables", "a.xlsx")]
+        public void ExportTableToExcelSql(string connectionString, string tableName, string fileName)
+        {
+            IReceive status = null;
+
+            IDataToSent data = null;
+            fileName = Guid.NewGuid().ToString("N") + fileName;
+
+            
+            $"the Excel {fileName} should not exists".w(() => File.Exists(fileName).Should().BeFalse());
+            $"Assume Sql Server instance {connectionString} exists , if not see docker folder".w(() => { });
+
+            $"When I create the ReceiveQueryFromDatabaseSql ".w(() =>
+                status = new ExportTableToExcelSql(connectionString, tableName, fileName));
+            $"and receive data".w(async () => { data = await status.TransformData(null); });
+            $"the data should have a table".w(() =>
+            {
+                data.DataToBeSentFurther.Count.Should().Be(3);
+                //table + 2 outputs
+            });
+
+
+            
+            $"the Excel {fileName} should  exists".w(() => File.Exists(fileName).Should().BeTrue());
 
 
 
