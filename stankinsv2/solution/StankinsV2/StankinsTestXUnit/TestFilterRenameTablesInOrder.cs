975358389eae95c1d7720aa475bb8b1b6f27b963
ignatandrei <ignatandrei@yahoo.com>
2019-02-05 01:12:43 +0200

    another test

---
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestFilterRenameTablesInOrder.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Stankins.File;
+using Stankins.Interfaces;
+using StankinsObjects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xbehave;
+using Xunit;
+using static System.Environment;
+namespace StankinsTestXUnit
+{
+    [Trait("FilterRenameTablesInOrder", "")]
+    [Trait("ExternalDependency", "0")]
+    public class TestFilterRenameTablesInOrder
+    {
+        [Scenario]
+        [Example("Year, Car{NewLine}Ford, 2000{NewLine}Rolls Royce, 2003",2,"test 0#")]
+        public void TestSimpleCSV(string fileContents,int nrStart,string format)
+        {
+            IReceive receiver = null;
+            IDataToSent data=null;
+            var nl = Environment.NewLine;
+            fileContents = fileContents.Replace("{NewLine}", nl);
+            $"When I create the receiver csv for the content {fileContents}".w(() => receiver = new ReceiverCSVText(fileContents));
+            $"And I read the data".w(async () =>data= await receiver.TransformData(null));
+            $"Then should be a data".w(() => data.Should().NotBeNull());
+            $"With a table".w(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().Be(1);
+            });
+            $"and applying FilterRenameTablesInOrder with {nrStart} and {format}".w(async () => data = await new FilterRenameTablesInOrder(nrStart, format).TransformData(data));
+
+            $"the name of the table should be {nrStart.ToString(format)}".w(() => data.DataToBeSentFurther[0].TableName = nrStart.ToString(format));
+        } 
+    }
+}
