264c9506093e007e1f0cb6d5139d9c285249f68c
ignatandrei <ignatandrei@yahoo.com>
2019-02-10 23:55:36 +0200

    another test for nameColumn

---
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestFilterColumnDataWithRegex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using Stankins.File;
+using Stankins.Interfaces;
+using StankinsObjects;
+using Xbehave;
+using Xunit;
+
+namespace StankinsTestXUnit
+{
+    [Trait("FilterColumnDataWithRegex","")]
+    [Trait("ExternalDependency", "0")]
+    public class TestFilterColumnDataWithRegex
+    {
+        [Scenario]
+        [Example("Car, Year{NewLine}Ford, 2000{NewLine}Rolls Royce, 2003",2,"[l]",1)]
+        public void TestSimpleCSV(string fileContents,int nrRows,string reg, int nrRowsAfter)
+        {
+            IReceive receiver = null;
+            IDataToSent data=null;
+            var nl = Environment.NewLine;
+            fileContents = fileContents.Replace("{NewLine}", nl);
+            $"When I create the receiver csv for the content {fileContents}".w(() => receiver = new ReceiverCSVText(fileContents));
+            $"And I read the data".w(async () =>data= await receiver.TransformData(null));
+            $"Then should be a data".w(() => data.Should().NotBeNull());
+            $"With a table".w(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().Be(1);
+            });
+            $"with {nrRows}".w(() => data.DataToBeSentFurther[0].Rows.Count.Should().Be(nrRows));
+
+            $" and after applying {nameof( FilterColumnDataWithRegex)} ".w(async ()=> data = await new FilterColumnDataWithRegex("Car","[l]").TransformData(data));
+
+            $"the name of the table should be {nrRowsAfter}".w(() =>data.DataToBeSentFurther[0].Rows.Count.Should().Be(nrRowsAfter));
+
+
+        } 
+    }
+}
--- stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestFilterRenameTablesInOrder.cs
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestFilterRenameTablesInOrder.cs
@@ -10,38 +10,43 @@ using System.Threading.Tasks;
 using Xbehave;
 using Xunit;
 using static System.Environment;
+
 namespace StankinsTestXUnit
 {
     [Trait("FilterRenameTablesInOrder", "")]
     [Trait("ChangeTableNamesRegex", "")]
+    [Trait("FilterColumnDataWithRegex", "")]
     [Trait("ExternalDependency", "0")]
     public class TestFilterRenameTablesInOrder
     {
         [Scenario]
-        [Example("Year, Car{NewLine}Ford, 2000{NewLine}Rolls Royce, 2003",2,"test 0#","0#")]
-        public void TestSimpleCSV(string fileContents,int nrStart,string format, string formatAfterRegex)
+        [Example("Year, Car{NewLine}Ford, 2000{NewLine}Rolls Royce, 2003", 2, "test 0#", "0#")]
+        public void TestSimpleCSV(string fileContents, int nrStart, string format, string formatAfterRegex)
         {
             IReceive receiver = null;
-            IDataToSent data=null;
+            IDataToSent data = null;
             var nl = Environment.NewLine;
             fileContents = fileContents.Replace("{NewLine}", nl);
-            $"When I create the receiver csv for the content {fileContents}".w(() => receiver = new ReceiverCSVText(fileContents));
-            $"And I read the data".w(async () =>data= await receiver.TransformData(null));
+            $"When I create the receiver csv for the content {fileContents}".w(() =>
+                receiver = new ReceiverCSVText(fileContents));
+            $"And I read the data".w(async () => data = await receiver.TransformData(null));
             $"Then should be a data".w(() => data.Should().NotBeNull());
             $"With a table".w(() =>
             {
                 data.DataToBeSentFurther.Should().NotBeNull();
                 data.DataToBeSentFurther.Count.Should().Be(1);
             });
-            $"and applying {nameof( FilterRenameTablesInOrder)} with {nrStart} and {format}".w(async () => data = await new FilterRenameTablesInOrder(nrStart, format).TransformData(data));
+            $"and applying {nameof(FilterRenameTablesInOrder)} with {nrStart} and {format}".w(async () =>
+                data = await new FilterRenameTablesInOrder(nrStart, format).TransformData(data));
 
-            $"the name of the table should be {nrStart.ToString(format)}".w(() => data.DataToBeSentFurther[0].TableName.Should().Be( nrStart.ToString(format)));
+            $"the name of the table should be {nrStart.ToString(format)}".w(() =>
+                data.DataToBeSentFurther[0].TableName.Should().Be(nrStart.ToString(format)));
 
-            $" and after applying {nameof( ChangeTableNamesRegex)} ".w(async ()=> data = await new ChangeTableNamesRegex("(?:.+ )((?<name>.+))").TransformData(data));
+            $" and after applying {nameof(ChangeTableNamesRegex)} ".w(async () =>
+                data = await new ChangeTableNamesRegex("(?:.+ )((?<name>.+))").TransformData(data));
 
-            $"the name of the table should be {nrStart}".w(() => data.DataToBeSentFurther[0].TableName.Should().Be(nrStart.ToString(formatAfterRegex)));
-
-        } 
-
+            $"the name of the table should be {nrStart}".w(() =>
+                data.DataToBeSentFurther[0].TableName.Should().Be(nrStart.ToString(formatAfterRegex)));
+        }
     }
 }
