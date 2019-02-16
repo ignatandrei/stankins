740d01c8fccf31f06a0982aeb0dfa224388387bb
ignatandrei <ignatandrei@yahoo.com>
2019-02-16 19:03:28 +0200

    reciepse simple

--- stankinsv2/solution/StankinsV2/StankinsAliveAngular/src/app/whatsimple/whatsimple.component.ts
+++ stankinsv2/solution/StankinsV2/StankinsAliveAngular/src/app/whatsimple/whatsimple.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../config.service';
 import { config } from 'rxjs';
 import { ResultTypeStankins, WhatToList } from './ResultTypeStankins';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-whatsimple',
@@ -11,20 +12,37 @@ import { ResultTypeStankins, WhatToList } from './ResultTypeStankins';
 })
 export class WhatsimpleComponent implements OnInit {
 
-  constructor(private cfg:ConfigService) { }
-  rs: ResultTypeStankins[] ;
-  rsRecipes : ResultTypeStankins[] ;
+  constructor(private cfg: ConfigService) { }
+  // rs: ResultTypeStankins[] ;
+  rsRecipes: ResultTypeStankins[] ;
+  
+  rsAll = new  Map<WhatToList, ResultTypeStankins[]>();
   ngOnInit() {
     this.cfg.GetStankinsAll().subscribe(it => {
-      this.rs = it;
+      // this.rs = it;
 
-      this.rsRecipes = it.filter((a)=>
-        (WhatToList.Receivers === (a.cacheWhatToList & WhatToList.Receivers)) &&
-        (WhatToList.Senders === (a.cacheWhatToList & WhatToList.Senders))
-      );
+      for (const value in WhatToList) {
+        if (typeof WhatToList[value] === 'number') {
+            console.log(WhatToList[value]);
+            const w: WhatToList = +WhatToList[value];
+            this.rsAll = this.rsAll.set(w, this.filterArr(it, w));
+        }
+
+        // const w: WhatToList  = what;
+        // this.rsAll.set(w, this.filterArr(it, w));
+     }
+     
+     const setReceivers = new Set(this.rsAll.get(WhatToList.Receivers));
+     const setSenders = new Set(this.rsAll.get(WhatToList.Senders));
+     this.rsRecipes = Array.from(setReceivers).filter(x => setSenders.has(x));
 
     });
   }
+  private filterArr(arr: ResultTypeStankins[] , w: WhatToList ): ResultTypeStankins[] {
 
+    const f = arr.filter(it => w === (it.cacheWhatToList & w));
+    console.log(`enter ${w} number ${f.length}`);
+    return f;
+  }
 }
 
