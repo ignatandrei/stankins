46c03b822928c0ff28a4ee66d60423996aa5c5ea
ignatandrei <ignatandrei@yahoo.com>
2019-02-17 20:19:55 +0200

    SenderOutputExcel

--- stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestExcel.cs
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestExcel.cs
@@ -12,13 +12,13 @@ using Xunit;
 using static System.Environment;
 namespace StankinsTestXUnit
 {
-    [Trait("SenderExcel", "")]
-    [Trait("ExternalDependency", "0")]
-    public class TestSenderExcel
+    public class TestExcel
     {
         [Scenario]
+        [Trait("SenderExcel", "")]
+        [Trait("ExternalDependency", "0")]
         [Example("Year, Car{NewLine}Ford, 2000{NewLine}Rolls Royce, 2003",2,"a.xlsx")]
-        public void TestSimpleCSVToExcel(string fileContents,int NumberRows,string fileName)
+        public void TestSimpleCSVSenderToExcel(string fileContents,int NumberRows,string fileName)
         {
             IReceive receiver = null;
             IDataToSent data=null;
@@ -39,5 +39,39 @@ namespace StankinsTestXUnit
                 data = await new SenderExcel(fileName).TransformData(data));
             $"the Excel {fileName} should exists".w(() => File.Exists(fileName).Should().BeTrue());
         } 
+        [Scenario]
+        [Trait("SenderOutputExcel", "")]
+        [Trait("ExternalDependency", "0")]
+        [Example("Year, Car{NewLine}Ford, 2000{NewLine}Rolls Royce, 2003",2,"a.xlsx")]
+        public void TestSimpleSenderOutputToExcel(string fileContents,int NumberRows,string fileName)
+        {
+            IReceive receiver = null;
+            IDataToSent data=null;
+            ISenderToOutput sender=null;
+            var nl = Environment.NewLine;
+            fileContents = fileContents.Replace("{NewLine}", nl);
+            fileName = Guid.NewGuid().ToString("N") + fileName;
+            $"the Excel {fileName} should not exists".w(() => File.Exists(fileName).Should().BeFalse());
+            $"When I create the receiver csv for the content {fileContents}".w(() => receiver = new ReceiverCSVText(fileContents));
+            $"And I read the data".w(async () =>data= await receiver.TransformData(null));
+            $"Then should be a data".w(() => data.Should().NotBeNull());
+            $"With a table".w(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().Be(1);
+            });
+            $"The number of rows should be {NumberRows}".w(() => data.DataToBeSentFurther[0].Rows.Count.Should().Be(NumberRows));
+            $"and now I want to export to excel file ".w(async () =>
+            {
+                sender=new SenderOutputExcel(fileName);
+                data = await sender.TransformData(data);
+            }
+                );
+            $"the Excel should exists".w(() => {
+                sender.OutputByte.Should().NotBeNull();
+                sender.OutputByte.Rows.Count.Should().Be(1);
+
+            });
+        }
     }
 }
