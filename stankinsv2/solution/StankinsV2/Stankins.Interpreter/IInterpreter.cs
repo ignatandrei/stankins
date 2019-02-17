58ac799b60add714c7df193557a8f2cc599a0dba
ignatandrei <ignatandrei@yahoo.com>
2019-02-17 12:19:27 +0200

    more data

---
+++ stankinsv2/solution/StankinsV2/Stankins.Interpreter/IInterpreter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using StankinsObjects;
+
+namespace Stankins.Interpreter
+{
+    public interface IInterpreter
+    {
+        bool CanInterpretString(string data);
+        BaseObject ObjectToRun();
+    }
+
+
+}
