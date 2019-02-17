9d9a9bc81f2916891a010090df09deaa75c2903b
ignatandrei <ignatandrei@yahoo.com>
2019-02-17 10:31:13 +0200

    transform xml

--- stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestReceiverXML.cs
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestReceiverXML.cs
@@ -3,7 +3,9 @@ using Stankins.File;
 using Stankins.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Stankins.XML;
@@ -14,11 +16,11 @@ namespace StankinsTestXUnit
 {
     [Trait("ReceiverCSV", "")]
     [Trait("ExternalDependency", "0")]
-    public class TestReceiverXML
+    public class TestXML
     {
         [Scenario]
         [Example("Assets/XML/nbrfxrates.xml",32)]
-        public void TestSimpleBNR(string fileName,int NumberRows)
+        public void TestReceiverSimpleBNR(string fileName,int NumberRows)
         {
             IReceive receiver = null;
             IDataToSent data=null;
@@ -27,7 +29,7 @@ namespace StankinsTestXUnit
             {
                 File.Exists(fileName).Should().BeTrue();
             });
-            $"When I create the receiver csv for the {fileName}".w(() => receiver = new ReceiverXML(fileName,null,"//*[name()='Rate']"));
+            $"When I create the r{nameof(ReceiverXML)} for the {fileName}".w(() => receiver = new ReceiverXML(fileName,null,"//*[name()='Rate']"));
 
             $"And I read the data".w(async () =>data= await receiver.TransformData(null));
             $"Then should be a data".w(() => data.Should().NotBeNull());
@@ -37,8 +39,48 @@ namespace StankinsTestXUnit
                 data.DataToBeSentFurther.Count.Should().Be(1);
             });
             $"The number of rows should be {NumberRows}".w(() => data.DataToBeSentFurther[0].Rows.Count.Should().Be(NumberRows));
+        } 
 
+        [Scenario]
+        [Example("Assets/XML/nbrfxrates.xml",32,"currency", "Value","4.7383")]
+        public void TestTransformerToColumns(string fileName,int NumberRows, string currencyName,string ValueName, string valueEur)
+        {
+            IReceive receiver = null;
+            IDataToSent data=null;
 
+            $"Given the file {fileName} ".w( () =>
+            {
+                File.Exists(fileName).Should().BeTrue();
+            });
+            $"When I create the {nameof(ReceiverXML)} for the {fileName}".w(() => receiver = new ReceiverXML(fileName,null,"//*[name()='Rate']"));
+
+            $"And I read the data".w(async () =>data= await receiver.TransformData(null));
+            $"Then should be a data".w(() => data.Should().NotBeNull());
+            $"With a table".w(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().Be(1);
+            });
+            $"The number of rows should be {NumberRows}".w(() => data.DataToBeSentFurther[0].Rows.Count.Should().Be(NumberRows));
+            $"And I transform  the data with {nameof(TransformerXMLToColumn)} for {currencyName}".w(async
+                () =>data= await new TransformerXMLToColumn("OuterXML",$"//@{currencyName}",currencyName,",").TransformData(data));
+
+            $"the table should contain {nameof(currencyName)}".w(() =>
+                data.Metadata.Columns.FirstOrDefault(it => it.Name == currencyName).Should().NotBeNull());
+
+            
+            $"the table should contain {nameof(ValueName)}".w(() =>
+                data.Metadata.Columns.FirstOrDefault(it => it.Name == ValueName).Should().NotBeNull());
+
+            $"and the data should contain for EUR {nameof(valueEur)}".w(() =>
+            {
+                DataView dv = new DataView(data.DataToBeSentFurther[0]);
+                dv.RowFilter = $"{currencyName}='EUR'";
+                dv.Count.Should().Be(1);
+                var row=dv[0].Row;
+                row[ValueName].ToString().Should().Be(valueEur);
+
+            });
         } 
     }
 }
