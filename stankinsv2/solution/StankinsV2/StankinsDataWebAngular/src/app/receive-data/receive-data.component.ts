1445e54f9fe91fb88198b288dcf4f5b6289f975c
ignatandrei <ignatandrei@yahoo.com>
2019-03-02 11:02:28 +0200

    more re

--- stankinsv2/solution/StankinsV2/StankinsDataWebAngular/src/app/receive-data/receive-data.component.ts
+++ stankinsv2/solution/StankinsV2/StankinsDataWebAngular/src/app/receive-data/receive-data.component.ts
@@ -9,41 +9,79 @@ import { finalize, tap, switchMap } from 'rxjs/operators';
   styleUrls: ['./receive-data.component.css']
 })
 export class ReceiveDataComponent implements OnInit {
-
   private dyn: DynamicControllers;
   constructor(private service: WritablesService) {
     this.dyn = new DynamicControllers(service);
   }
 
   ngOnInit() {
-
     this.dyn.onInit().subscribe(it => window.alert(`Number loaded ${it}`));
   }
-  save(item: KeyValuePair ) {
+  save(item: KeyValuePair) {
     this.dyn.save(item);
+  }
+  addNew() {
+   // window.alert(this.dyn.kv.length);
+    this.dyn.addNew();
+   // window.alert(this.dyn.kv.length);
   }
 }
 
 export class DynamicControllers {
+  kv = new Array<KeyValuePair>();
 
-  kv = new  Array<KeyValuePair>();
-
-  constructor(private service: WritablesService) { }
+  constructor(private service: WritablesService) {}
 
   onInit(): Observable<number> {
     const self = this;
     return this.service.GetAll().pipe(
-
-      tap(it => self.kv = it),
+      tap(it => (self.kv = it)),
 
       switchMap(it => of(it.length))
-
     );
   }
   save(item: KeyValuePair) {
-    this.service.Save(item).subscribe(
-      () => this.onInit().subscribe()
-    );
+    this.service.Save(item).subscribe(() => this.onInit().subscribe());
+  }
+  private newGuid(): string {
+    let result: string;
+    let i: string;
+    let j: number;
 
+    result = '';
+    for (j = 0; j < 32; j++) {
+      if (j === 8 || j === 12 || j === 16 || j === 20) {
+        result = result + '';
+      }
+      i = Math.floor(Math.random() * 16)
+        .toString(16)
+        .toUpperCase();
+      result = result + i;
+    }
+    return result;
+  }
+  addNew() {
+    const kv = new KeyValuePair();
+    kv.key = this.newGuid();
+    kv.value = `using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace StankinsDataWeb.Controllers
+    {
+        [Route("api/v{version:apiVersion}/[controller]")]
+        [ApiVersion( "1.0" )]
+        [ApiController]
+        public class Controller_${kv.key} : ControllerBase
+        {
+            [HttpGet]
+            public string Get()
+            {
+                return "Andrei" ;
+            }
+        }
+    }`;
+    this.kv.push(kv);
   }
 }
--- stankinsv2/solution/StankinsV2/StankinsDataWebAngular/src/app/writables.service.ts
+++ stankinsv2/solution/StankinsV2/StankinsDataWebAngular/src/app/writables.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-export interface KeyValuePair {
+export class KeyValuePair {
   key: string;
   value: string;
 }
