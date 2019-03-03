1efe7c617d4f5313cc0077588e0ab0de9ec13024
ignatandrei <ignatandrei@yahoo.com>
2019-03-03 06:28:54 +0200

    more trello

--- stankinsv2/solution/StankinsV2/Stankins.Trello/ReceiverFromTrello.cs
+++ stankinsv2/solution/StankinsV2/Stankins.Trello/ReceiverFromTrello.cs
@@ -21,11 +21,29 @@ namespace Stankins.Trello
             var cards= trello.Cards;
             var lists=trello.Lists;
             
+            var dtList=new DataTable();
+            dtList.Columns.Add("id",typeof(string));
+            dtList.Columns.Add("name",typeof(string));
+            dtList.Columns.Add("idboard",typeof(string));
+
+            var dtCards=new DataTable();
+            dtCards.Columns.Add("id",typeof(string));
+            dtCards.Columns.Add("name",typeof(string));
+            dtCards.Columns.Add("idlist",typeof(string));
+            
+            
             foreach(var l in lists)
             {
+                dtList.Rows.Add(l.Id,l.Name,l.IdBoard);
+            }
+            yield return dtList;
+            foreach (var c in cards)
+            {
+                dtCards.Rows.Add(c.Id,c.Name,c.IdList);
                 
             }
-            return null;
+
+            yield return dtCards;
 
         }
 
