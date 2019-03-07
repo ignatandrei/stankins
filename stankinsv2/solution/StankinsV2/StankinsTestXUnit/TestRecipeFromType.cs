aa994f0b336b2242aaef2deda2120243ecda4f39
ignatandrei <ignatandrei@yahoo.com>
2019-03-07 08:12:55 +0200

    asdfas

--- stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestRecipeFromType.cs
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestRecipeFromType.cs
@@ -1,11 +1,40 @@
 ﻿using FluentAssertions;
+using Stankins.Interfaces;
 using Stankins.Interpreter;
+using Stankins.Rest;
 using StankinsHelperCommands;
 using System.Linq;
 using Xbehave;
 using Xunit;
 namespace StankinsTestXUnit
 {
+    [Trait("TestRecipeFromType", "")]
+    [Trait("ExternalDependency", "0")]
+    public class TestReceiverRestFromText 
+    {
+        [Scenario]
+        [Example("{id:1,name:'andrei'}",2,1)]
+        public void TestSimple(string text,int nrCols,int nrRows)
+        {
+            IReceive r=null;
+            IDataToSent data=null;
+            $"when creating receiving ReceiverRestFromText ".w(async ()=>
+            { 
+                r=new ReceiverRestFromText(text ); 
+                data=await r.TransformData(null);
+            }
+            );
+            $"then should be 1 table".w(()=>
+                data.DataToBeSentFurther.Count().Should().Be(1)
+            );
+            $"then should be {nrCols} cols ".w(()=>
+                data.DataToBeSentFurther[0].Columns.Count.Should().Be(nrCols)
+            );
+            $"then should be {nrRows} cols ".w(()=>
+                data.DataToBeSentFurther[0].Rows.Count.Should().Be(nrRows)
+            );
+        }
+    }
     [Trait("TestRecipeFromType", "")]
     [Trait("ExternalDependency", "0")]
     public class TestRecipeFromType
