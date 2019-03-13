348343a9133a5ce4a60621e69ee6cfaa1d8956be
ignatandrei <ignatandrei@yahoo.com>
2019-03-13 07:08:35 +0200

    verifying not duplicats

--- stankinsv2/solution/StankinsV2/Stankins.Console/Program.cs
+++ stankinsv2/solution/StankinsV2/Stankins.Console/Program.cs
@@ -1,49 +1,34 @@
 ﻿
+using Microsoft.Extensions.CommandLineUtils;
+using Stankins.FileOps;
+using Stankins.Interfaces;
+using Stankins.Interpreter;
+using Stankins.SimpleRecipes;
+using StankinsCommon;
+using StankinsHelperCommands;
+using StankinsObjects;
 using System;
 using System.Collections.Generic;
-using Microsoft.Extensions.CommandLineUtils;
-using Stankins.Interfaces;
-using Stankins.Razor;
-using Stankins.SqlServer;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using NPOI.HSSF.Record;
-using NPOI.HSSF.Record.Cont;
-using NPOI.OpenXmlFormats.Wordprocessing;
-using Stankins.AnalyzeSolution;
-using Stankins.FileOps;
-using Stankins.Office;
-using StankinsCommon;
-using StankinsObjects;
-using StankinsHelperCommands;
-using Stankins.AzureDevOps;
-using System.Threading.Tasks;
-using Stankins.Process;
-using Stankins.Rest;
-using Stankins.Version;
-using Stankins.XML;
-using Stankins.SimpleRecipes;
-using Stankins.Interpreter;
 
 namespace Stankins.Console
 {
     //https://gist.github.com/iamarcel/8047384bfbe9941e52817cf14a79dc34
-    class Program
+    internal class Program
     {
-
-        static string ExtendedHelpText()
+        private static string ExtendedHelpText()
         {
             string connectionString = "\"Server=..;Database=..;User Id=...;Password=...\"";
             //docker example
             //connectionString = "\"Server=(local);Database=tests;User Id=SA;Password = <YourStrong!Passw0rd>;\"";
             string nl = Environment.NewLine;
-            var sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             sb.AppendLine("");
             sb.AppendLine("Some fast usage  ");
-            
-            
+
+
             sb.AppendLine("SqlServer:");
             sb.AppendLine(
                 $"     SqlServer1)To export diagram :{nl} stankins.console execute -o {nameof(ExportDBDiagramHtmlAndDot)} -a {connectionString} -a metadata.html");
@@ -60,16 +45,15 @@ namespace Stankins.Console
 
         }
 
-        
-        static void WriteLines(ResultTypeStankins[] b)
+        private static void WriteLines(ResultTypeStankins[] b)
         {
-            foreach (var it in b)
+            foreach (ResultTypeStankins it in b)
             {
                 Type t = it.Type;
                 System.Console.WriteLine("");
                 System.Console.Write($"stankins.console execute -o {t.Name}");
 
-                foreach (var item in it.ConstructorParam)
+                foreach (KeyValuePair<string, object> item in it.ConstructorParam)
                 {
                     System.Console.Write($" -a {item.Key}");
                 }
@@ -78,29 +62,40 @@ namespace Stankins.Console
 
             };
         }
-        
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
 
-            var refs= FindAssembliesToExecute.AddReferences();
+            ResultTypeStankins[] refs = FindAssembliesToExecute.AddReferences();
 
-            var commands = new CtorDictionaryGeneric<ResultTypeStankins>();
+            CtorDictionaryGeneric<ResultTypeStankins> commands = new CtorDictionaryGeneric<ResultTypeStankins>();
 
-            Action<ResultTypeStankins> createItem = (t) => { commands.Add(t.Name, t); };
-            foreach (var item in refs)
+            Action<ResultTypeStankins> createItem = (t) =>
+            {
+                if (commands.ContainsKey(t.Name)){
+                    System.Console.WriteLine($"key exists : {t.Name}");
+                }
+                else
+                {
+                    commands.Add(t.Name, t);
+                }
+            };
+            foreach (ResultTypeStankins item in refs)
             {
                 createItem(item);
             }
-          
 
-            var app = new CommandLineApplication();
-            app.Name = "Stankins.Console";
-            var versionString = Assembly.GetEntryAssembly()
+
+            CommandLineApplication app = new CommandLineApplication
+            {
+                Name = "Stankins.Console"
+            };
+            string versionString = Assembly.GetEntryAssembly()
                 .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
                 .InformationalVersion
                 .ToString();
 
-           
+
 
             app.HelpOption("-?|-h|--help");
             app.VersionOption("-v|--version", app.Name + "v" + versionString, app.Name + "v" + versionString);
@@ -121,17 +116,18 @@ namespace Stankins.Console
             //    });
 
             //});
-            app.Command("recipes",(command)=>{
-                command.Description = "execute/list recipes already in system" ;
-                var list=command.Option("-l|--list","list recipes",CommandOptionType.NoValue);
-                var execute=command.Option("-e|--execute","execute recipe",CommandOptionType.SingleValue);
+            app.Command("recipes", (command) =>
+            {
+                command.Description = "execute/list recipes already in system";
+                CommandOption list = command.Option("-l|--list", "list recipes", CommandOptionType.NoValue);
+                CommandOption execute = command.Option("-e|--execute", "execute recipe", CommandOptionType.SingleValue);
                 command.OnExecute(async () =>
                 {
-                    if(list.HasValue())
+                    if (list.HasValue())
                     {
-                        var recipes = RecipeFromString.RecipesFromFolder().ToList();
-                        recipes.Sort((a,b)=>a.Name.CompareTo(b.Name));
-                        foreach(var item in recipes)
+                        List<Recipe> recipes = RecipeFromString.RecipesFromFolder().ToList();
+                        recipes.Sort((a, b) => a.Name.CompareTo(b.Name));
+                        foreach (Recipe item in recipes)
                         {
                             System.Console.WriteLine($"Stankins.Console recipes -e {item.Name}");
                         }
@@ -139,13 +135,13 @@ namespace Stankins.Console
                     }
                     if (execute.HasValue())
                     {
-                        var recipeString=  execute.Value();
-                        var recipe=RecipeFromString.FindRecipe(recipeString);
-                        if(recipe == null)
+                        string recipeString = execute.Value();
+                        RecipeFromString recipe = RecipeFromString.FindRecipe(recipeString);
+                        if (recipe == null)
                         {
                             System.Console.Error.WriteLine($"can not found {recipeString}");
                             return 0;//maybe return error?
-                        }   
+                        }
                         await recipe.TransformData(null);
                         return 0;
 
@@ -158,28 +154,28 @@ namespace Stankins.Console
             });
             app.Command("list", (command) =>
             {
-                var names=string.Join(',', Enum.GetNames(typeof(WhatToList)));
+                string names = string.Join(',', Enum.GetNames(typeof(WhatToList)));
 
                 command.Description = "Explain arguments  for supported objects - could be " + names;
 
-                command.HelpOption("-?|-h|--help"); 
-                var optWhat = command.Option("-what", "what to list", CommandOptionType.SingleValue);
-                
+                command.HelpOption("-?|-h|--help");
+                CommandOption optWhat = command.Option("-what", "what to list", CommandOptionType.SingleValue);
+
 
 
                 command.OnExecute(() =>
                 {
                     if (!optWhat.HasValue())
                     {
-                        System.Console.WriteLine("please add -what " +names);
+                        System.Console.WriteLine("please add -what " + names);
                         return -1;
                     }
-                    var val =(WhatToList) (int) Enum.Parse(typeof(WhatToList), optWhat.Value(),true);
-                    
-                    var all = commands.Select(it => it.Value).ToList();
+                    WhatToList val = (WhatToList)(int)Enum.Parse(typeof(WhatToList), optWhat.Value(), true);
 
-                    var find = all.Where(it => val == (val & it.FromType())).ToList();
-                    find.Sort((a,b)=>a.Name.CompareTo(b.Name));
+                    List<ResultTypeStankins> all = commands.Select(it => it.Value).ToList();
+
+                    List<ResultTypeStankins> find = all.Where(it => val == (val & it.FromType())).ToList();
+                    find.Sort((a, b) => a.Name.CompareTo(b.Name));
 
                     WriteLines(find.ToArray());
 
@@ -196,8 +192,8 @@ namespace Stankins.Console
                 {
                     command.Description = "Execute multiple ";
                     command.HelpOption("-?|-h|--help");
-                    var opt = command.Option("-o", "execute object", CommandOptionType.MultipleValue);
-                    var argObjects = command.Option("-a", "arguments for the object", CommandOptionType.MultipleValue);
+                    CommandOption opt = command.Option("-o", "execute object", CommandOptionType.MultipleValue);
+                    CommandOption argObjects = command.Option("-a", "arguments for the object", CommandOptionType.MultipleValue);
                     //var locationArgument = command.Argument("[location]", "The object to execute -see list command ", true);
 
                     command.OnExecute(async () =>
@@ -209,12 +205,12 @@ namespace Stankins.Console
                         }
 
 
-                        var argNr = 0;
-                        var lenValuesCount = opt.Values.Count;
+                        int argNr = 0;
+                        int lenValuesCount = opt.Values.Count;
                         for (int i = 0; i < lenValuesCount; i++)
                         {
 
-                            var item = opt.Values[i].ToLowerInvariant();
+                            string item = opt.Values[i].ToLowerInvariant();
                             if (!commands.ContainsKey(item))
                             {
                                 System.Console.WriteLine($"not an existing object {item} - please see list command");
@@ -228,10 +224,10 @@ namespace Stankins.Console
                         {
 
                             System.Console.WriteLine($"not equal nr args -a {lenValuesCount} with  nr args for objects {argNr} - please see list command");
-                            
+
                             return -1;
                         }
-                    
+
 
                         IBaseObject last = null;
                         IDataToSent data = null;
@@ -239,8 +235,8 @@ namespace Stankins.Console
                         for (int value = 0; value < lenValuesCount; value++)
                         {
 
-                            var item = opt.Values[value].ToLowerInvariant();
-                            var cmd = commands[item];
+                            string item = opt.Values[value].ToLowerInvariant();
+                            ResultTypeStankins cmd = commands[item];
                             object[] ctorArgs = null;
                             if (cmd.ConstructorParam.Count > 0)
                             {
@@ -248,32 +244,33 @@ namespace Stankins.Console
                                 int i = 0;
                                 do
                                 {
-                                    var item1= argObjects.Values[argNr];
-                                    
-                                    ctorArgs[i] = item1; 
+                                    string item1 = argObjects.Values[argNr];
+
+                                    ctorArgs[i] = item1;
                                     i++;
                                     argNr++;
 
                                 } while (i < cmd.ConstructorParam.Count);
                             }
-                            
+
                             last = cmd.Create(ctorArgs);
                             data = await last.TransformData(data);
 
 
                         }
 
-                       
-                        var output = last as ISenderToOutput;
+
+                        ISenderToOutput output = last as ISenderToOutput;
                         if (output != null)
                         {
                             System.Console.WriteLine("exporting default output");
-                            var sender = new SenderOutputToFolder("", true);
-                            data=await sender.TransformData(data);
-                        
-                        }else
+                            SenderOutputToFolder sender = new SenderOutputToFolder("", true);
+                            data = await sender.TransformData(data);
+
+                        }
+                        else
                         {
-                            
+
                             System.Console.WriteLine("exporting all tables to csv");
                             await new SenderAllTablesToFileCSV("").TransformData(data);
 
