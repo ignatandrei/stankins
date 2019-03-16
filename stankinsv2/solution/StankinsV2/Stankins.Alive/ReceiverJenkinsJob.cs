dafc162e628eaf515f8ba6fdf0e01ba8cb4e870c
ignatandrei <ignatandrei@yahoo.com>
2019-03-17 01:16:44 +0200

    jenkins job

---
+++ stankinsv2/solution/StankinsV2/Stankins.Alive/ReceiverJenkinsJob.cs
@@ -0,0 +1,57 @@
+﻿using Stankins.Interfaces;
+using Stankins.Jenkins;
+using Stankins.Rest;
+using StankinsCommon;
+using StankinsObjects;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Stankins.Alive
+{
+    public class ReceiverJenkinsJob : AliveStatus, IReceive
+    {
+        private readonly string url;
+        private readonly string jobName;
+
+        public ReceiverJenkinsJob(string url, string jobName) : this(new CtorDictionary()
+        {
+            {nameof(url),url },
+              {nameof(jobName),jobName }
+
+            })
+        {
+          
+        }
+        public ReceiverJenkinsJob(CtorDictionary dataNeeded) : base(dataNeeded)
+        {
+            url = GetMyDataOrThrow<string>(nameof(url));
+            jobName = GetMyDataOrThrow<string>(nameof(jobName));
+            Name = nameof(ReceiverJenkins);
+        }
+        public override async Task<IDataToSent> TransformData(IDataToSent receiveData)
+        {
+            if (receiveData == null)
+            {
+                receiveData = new DataToSentTable();
+            }
+            var jobUrl= url+"/job/"+jobName+"/api/json";
+            var res = new ReceiveRestFromFile(jobUrl);
+            var results = CreateTable(receiveData);
+            var sw = Stopwatch.StartNew();
+            DateTime StartedDate = DateTime.UtcNow;
+            try
+            {
+                var data = await res.TransformData(receiveData);
+                results.Rows.Add("jenkinsjob", "/job/"+jobName+"/api/json", url, true, "", sw.ElapsedMilliseconds, "", null, StartedDate);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                results.Rows.Add("jenkinsjob", "", url, false, null, sw.ElapsedMilliseconds, null, ex.Message, StartedDate);
+
+            }
+            return receiveData;
+        }
+    }
+}
