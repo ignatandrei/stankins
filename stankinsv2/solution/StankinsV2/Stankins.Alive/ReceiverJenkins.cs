08756b0ab2a5f59142898e6053b20f2ab8ed5a70
ignatandrei <ignatandrei@yahoo.com>
2019-03-16 23:45:08 +0200

    added jenkins alive

---
+++ stankinsv2/solution/StankinsV2/Stankins.Alive/ReceiverJenkins.cs
@@ -0,0 +1,53 @@
+﻿using Stankins.Interfaces;
+using Stankins.Jenkins;
+using StankinsCommon;
+using StankinsObjects;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Stankins.Alive
+{
+    public class ReceiverJenkins : AliveStatus, IReceive
+    {
+        private readonly string url;
+
+        public ReceiverJenkins(string url) : this(new CtorDictionary()
+        {
+            {nameof(url),url }
+
+            })
+        {
+            this.url = url;
+        }
+        public ReceiverJenkins(CtorDictionary dataNeeded) : base(dataNeeded)
+        {
+            url = GetMyDataOrThrow<string>(nameof(url));
+            Name = nameof(ReceiverJenkins);
+        }
+        public override async Task<IDataToSent> TransformData(IDataToSent receiveData)
+        {
+            if (receiveData == null)
+            {
+                receiveData = new DataToSentTable();
+            }
+
+            JenkinsJson res = new JenkinsJson(url);
+            System.Data.DataTable results = CreateTable(receiveData);
+            Stopwatch sw = Stopwatch.StartNew();
+            DateTime StartedDate = DateTime.UtcNow;
+            try
+            {
+                IDataToSent data = await res.TransformData(receiveData);
+                results.Rows.Add("jenkinssite", "/api/json", url, true, "", sw.ElapsedMilliseconds, "", null, StartedDate);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                results.Rows.Add("jenkinssite", "", url, false, null, sw.ElapsedMilliseconds, null, ex.Message, StartedDate);
+
+            }
+            return receiveData;
+        }
+    }
+}
--- stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestJenkins.cs
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/TestJenkins.cs
@@ -1,18 +1,12 @@
 ﻿using FluentAssertions;
-using Stankins.FileOps;
+using Stankins.Alive;
 using Stankins.Interfaces;
+using Stankins.Jenkins;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
-using Stankins.Rest;
+using System.Linq;
 using Xbehave;
 using Xunit;
-using static System.Environment;
-using Stankins.Trello;
-using Stankins.Cachet;
-using Stankins.Jenkins;
 
 namespace StankinsTestXUnit
 {
@@ -22,23 +16,50 @@ namespace StankinsTestXUnit
     {
         [Scenario]
         [Example("http://localhost:8080", 3)]
-        public void TestSimpleJSON(string url,int numberTables)
+        public void TestSimpleJSON(string url, int numberTables)
         {
             IReceive receiver = null;
-           
-            IDataToSent data=null;
-            var nl = Environment.NewLine;
-           
+
+            IDataToSent data = null;
+            string nl = Environment.NewLine;
+
             $"When I create the {nameof(JenkinsJson)} for the {url}".w(() => receiver = new JenkinsJson(url));
-            $"And I read the data".w(async () =>data= await receiver.TransformData(null));
+            $"And I read the data".w(async () => data = await receiver.TransformData(null));
             $"Then should be a data".w(() => data.Should().NotBeNull());
             $"With {numberTables} table".w(() =>
             {
                 data.DataToBeSentFurther.Should().NotBeNull();
-                data.DataToBeSentFurther.Count.Should().Be(numberTables);
+                data.DataToBeSentFurther.Count.Should().BeGreaterOrEqualTo(numberTables);
             });
 
-        } 
+        }
+
+        [Scenario]
+        [Example("http://localhost:8080", 1)]
+        public void TestAlive(string url, int numberTables)
+        {
+            IReceive receiver = null;
+
+            IDataToSent data = null;
+            string nl = Environment.NewLine;
+
+            $"When I create the {nameof(ReceiverJenkins)} for the {url}".w(() => receiver = new ReceiverJenkins(url));
+            $"And I read the data".w(async () => data = await receiver.TransformData(null));
+            $"Then should be a data".w(() => data.Should().NotBeNull());
+            $"With {numberTables} table".w(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().BeGreaterOrEqualTo(numberTables);
+            });
+            $"and should have data success".w(() =>
+            {
+                var res = AliveStatus.FromTable(data.DataToBeSentFurther.First().Value);
+                res.Should().NotBeNull();
+                res.Count().Should().Be(1);
+                res.First().IsSuccess.Should().Be(true);
+            });
+
+        }
 
     }
 }
