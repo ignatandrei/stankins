97f934376a1c465d0c4d76e0f374b1be7aa93771
ignatandrei <ignatandrei@yahoo.com>
2019-03-16 18:30:18 +0200

    docker run sql server inside

--- stankinsv2/solution/StankinsV2/StankinsTestXUnit/Docker/startsqlServer.bat
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/Docker/startsqlServer.bat
@@ -1,9 +1,9 @@
-echo windows
-docker-compose -f docker-sqlserver-instance-windows.yaml up -d   
+rem echo windows
+rem docker-compose -f docker-sqlserver-instance-windows.yaml up -d   
 echo linux
 docker-compose -f docker-sqlserver-instance-linux.yaml up -d   
 docker container ls 
 pause
-docker-compose -f docker-sqlserver-instance-windows.yaml down   
+rem docker-compose -f docker-sqlserver-instance-windows.yaml down   
 docker-compose -f docker-sqlserver-instance-linux.yaml down   
             
