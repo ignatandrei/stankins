718c458fb2160eafdd100ba731eebd8e05006080
Andrei Ignat <ignatandrei@yahoo.com>
2019-03-27 00:35:33 +0200

    trying to compile on Linux

--- stankinsv2/DeveloperTestStankins.bat
+++ stankinsv2/DeveloperTestStankins.bat
@@ -1,8 +1,8 @@
 cls
 ECHO please be aware of absolute path here %cd%
-docker build -t stankins_test_image -f DeveloperTestStankins.txt .
+docker build --no-cache -t stankins_test_image -f DeveloperTestStankins.txt .
 docker-compose -f solution/StankinsV2/StankinsTestXUnit/Docker/docker-sqlserver-instance-linux.yaml up -d 
 echo start powershell
 Start powershell ./all.ps1
-docker run -it --name stankins_test_container --rm stankins_test_image  dotnet watch -p StankinsTestXUnit/StankinsTestXUnit.csproj test
+docker run -it --name stankins_test_container --rm stankins_test_image  dotnet watch   -p StankinsTestXUnit/StankinsTestXUnit.csproj test -v n
 docker-compose -f solution/StankinsV2/StankinsTestXUnit/Docker/docker-sqlserver-instance-linux.yaml down 
--- stankinsv2/all.ps1
+++ stankinsv2/all.ps1
@@ -9,19 +9,25 @@ while($TRUE){
 	if($result.TimedOut){
 		continue;
 	}
+	$cmd = "docker cp  solution/StankinsV2/. stankins_test_container:/usr/app/"
+    Write-Host $cmd
+    Invoke-Expression -Command $cmd
+            
     switch($result.ChangeType){
         Changed{
         #TODO: see if folder or file
-            $fullName = [System.IO.Path]::Combine($watcher.Path ,$result.Name)          
-            $cmd= "docker cp $fullName stankins_test_container:/usr/app/"+$result.Name.Replace("\","/")
-            Write-Host $cmd
-            Invoke-Expression -Command $cmd
-            
+            $fullName = [System.IO.Path]::Combine($watcher.Path ,$result.Name) 
+			$pathOnDocker= $result.Name.Replace("\","/").Replace("solution/StankinsV2/","")
+			$fullName = [System.IO.Path]::GetDirectoryName($fullName)
+			$pathOnDocker= [System.IO.Path]::GetDirectoryName($pathOnDocker)
+            $cmd= "docker cp $fullName stankins_test_container:/usr/app/"+$pathOnDocker
+			
 
         }
         default{
         	write-host "not handled " $result.ChangeType + " Change in "  + $result.Name
-        }
+			write-host " end"
+	   }
     }
 
     
