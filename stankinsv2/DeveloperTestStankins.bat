3887a47927ad149681d58d10f54de918e55f6fdb
Andrei Ignat <ignatandrei@yahoo.com>
2019-03-27 09:04:53 +0200

    running tests

--- stankinsv2/DeveloperTestStankins.bat
+++ stankinsv2/DeveloperTestStankins.bat
@@ -3,6 +3,8 @@ ECHO please be aware of absolute path here %cd%
 docker build --no-cache -t stankins_test_image -f DeveloperTestStankins.txt .
 docker-compose -f solution/StankinsV2/StankinsTestXUnit/Docker/docker-sqlserver-instance-linux.yaml up -d 
 echo start powershell
-Start powershell ./all.ps1
-docker run -it --name stankins_test_container --rm stankins_test_image  dotnet watch   -p StankinsTestXUnit/StankinsTestXUnit.csproj test -v n
+Start powershell ./all.ps1 
+REM -v n
+REM docker run --network docker_default -m 3G --memory-reservation 2G -it --name stankins_test_container --rm stankins_test_image  dotnet watch   -p StankinsTestXUnit/StankinsTestXUnit.csproj test -c release 
+docker run --network docker_default -m 3G --memory-reservation 2G -it --name stankins_test_container --rm stankins_test_image  
 docker-compose -f solution/StankinsV2/StankinsTestXUnit/Docker/docker-sqlserver-instance-linux.yaml down 
