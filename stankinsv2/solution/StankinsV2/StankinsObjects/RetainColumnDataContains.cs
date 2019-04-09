b66c07dabf975dedb696237d4a4891e3b9bdd3a1
Andrei Ignat <ignatandrei@yahoo.com>
2019-04-09 08:20:09 +0300

    changed tests for console

--- stankinsv2/solution/StankinsV2/StankinsObjects/RetainColumnDataContains.cs
+++ stankinsv2/solution/StankinsV2/StankinsObjects/RetainColumnDataContains.cs
@@ -1,18 +1,27 @@
-﻿using StankinsCommon;
+﻿using Newtonsoft.Json.Linq;
+using StankinsCommon;
 
 namespace StankinsObjects
 {
     public class FilterRetainColumnDataContains: FilterColumnData
     {
+        private readonly string nameColumn;
+        private readonly string stringToHave;
+
         public FilterRetainColumnDataContains(string nameColumn, string stringToHave) : this(new CtorDictionary() {
             { nameof(nameColumn), nameColumn },
-            { nameof(Expression), $"not({nameColumn} like '%{stringToHave}%')" }
+            { nameof(stringToHave), stringToHave }
             }
           )
         {
+            this.nameColumn = nameColumn;
+            this.stringToHave = stringToHave;
             
         }
-        public FilterRetainColumnDataContains(CtorDictionary dataNeeded) : base(dataNeeded)
+        public FilterRetainColumnDataContains(CtorDictionary dataNeeded) : base(
+            new CtorDictionary(dataNeeded)
+            .AddMyValue(nameof(Expression), $"not({dataNeeded[nameof(nameColumn)]} like '%{dataNeeded[nameof(stringToHave)]}%')")
+            )
         {
             this.Name = nameof(FilterRetainColumnDataContains);
         }
---
+++ stankinsv2/solution/StankinsV2/StankinsTestXUnit/VariousTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Stankins.Interfaces;
+using Stankins.Interpreter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xbehave;
+using Xunit;
+
+namespace StankinsTestXUnit
+{
+    
+    
+    public class VariousTests
+    {
+        [Scenario]
+        [Trait("ExternalDependency", "YouTube")]
+        public void AndreiPlaylist()
+        {
+            IReceive r = null;
+            IDataToSent data = null;
+            $"when I create a recipe with jenkins".w(() => r = new RecipeFromFilePath("Assets/recipes/youtubeplaylist.txt"));
+            $"and I transform".w(async () => data= await r.TransformData(null));
+            $" there should be >50 links".w(() =>
+            {
+                data.DataToBeSentFurther.Should().NotBeNull();
+                data.DataToBeSentFurther.Count.Should().Be(2);
+                var sumRows = data.DataToBeSentFurther.Select(it => it.Value.Rows.Count).Sum();
+                sumRows.Should().BeGreaterThan(50);
+            });
+        }
+    }
+}
