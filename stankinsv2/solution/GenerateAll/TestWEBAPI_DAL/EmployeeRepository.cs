c73afd9f0491d82ba969d3bcd7f86ea3e2e1932c
ignatandrei <ignatandrei@yahoo.com>
2020-07-27 15:44:34 +0300

    emp c

--- stankinsv2/solution/GenerateAll/TestWEBAPI_DAL/EmployeeRepository.cs
+++ stankinsv2/solution/GenerateAll/TestWEBAPI_DAL/EmployeeRepository.cs
@@ -4,7 +4,9 @@
     var dt= Model.DataToBeSentFurther[0];
     string repo= @dt.TableName  + "_Repository";
 }
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,32 +23,42 @@ namespace TestWebAPI_DAL
         {
             this.databaseContext = databaseContext;
         }
-        public Task<@dt.TableName []> GetAll_@dt.TableName ()
+        public Task<@(dt.TableName)[]> GetAll_@dt.TableName ()
         {
-            return databaseContext.@dt.TableName .ToArrayAsync();
+            return databaseContext.@(dt.TableName).ToArrayAsync();
         }
-        public Task<@dt.TableName > FindAfterId(long id)
+        public Task<@dt.TableName> FindAfterId(long id)
         {
-            var data = databaseContext. @dt.TableName  .FirstOrDefaultAsync(it => it.ID == id);
+            var data = databaseContext.@(dt.TableName).FirstOrDefaultAsync(it => it.ID == id);
             return data;
         }
-        public async Task<@dt.TableName > Insert(@dt.TableName p)
+        public Task<@dt.TableName> FindSingle(Func<@dt.TableName ,bool> f)
         {
-            databaseContext.People.Add(p);
+            var data = databaseContext.@(dt.TableName).FirstOrDefaultAsync(it=>f(it));
+            return data;
+        }
+        public Task<@(dt.TableName)[]> FindMultiple(Func<@dt.TableName, bool> f)
+        {
+            var data = databaseContext.@(dt.TableName).Where(it=>f(it));
+            return data.ToArrayAsync();
+        }
+        public async Task<@dt.TableName> Insert(@dt.TableName p)
+        {
+            databaseContext.@(dt.TableName).Add(p);
             await databaseContext.SaveChangesAsync();
             return p;
         }
-        public async Task<@dt.TableName > Update(@dt.TableName p)
+        public async Task<@dt.TableName> Update(@dt.TableName p)
         {
             var original = await FindAfterId(p.ID);
             original.CopyPropertiesFrom(other: p, withID: true);                        
             await databaseContext.SaveChangesAsync();
             return p;
         }
-        public async Task<@dt.TableName > Delete(@dt.TableName p)
+        public async Task<@dt.TableName> Delete(@dt.TableName p)
         {
             var original = await FindAfterId(p.ID);
-            databaseContext.People.Remove(original);
+            databaseContext.@(dt.TableName).Remove(original);
             await databaseContext.SaveChangesAsync();
             return p;
         }
