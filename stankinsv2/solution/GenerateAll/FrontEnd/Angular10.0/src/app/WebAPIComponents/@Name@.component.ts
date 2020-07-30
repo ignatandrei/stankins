714d53c22dc60cbbe754dce607b7fc29f7781fcc
ignatandrei <ignatandrei@yahoo.com>
2020-07-30 10:36:29 +0300

    more n

--- stankinsv2/solution/GenerateAll/FrontEnd/Angular10.0/src/app/WebAPIComponents/@Name@.component.ts
+++ stankinsv2/solution/GenerateAll/FrontEnd/Angular10.0/src/app/WebAPIComponents/@Name@.component.ts
@@ -15,11 +15,12 @@ import {@(nameTable)Service} from './../services/@(nameTable).service';
 })
 export class @Name@Component implements OnInit {
 
+  public data:@(nameTable)[]=[];
   constructor(private mainService: @(nameTable)Service ) { }
 
   ngOnInit(): void {
 	  this.mainService.GetAll().subscribe(it=>{
-      window.alert(it.length);
+		data=it;
     })
   }
 
