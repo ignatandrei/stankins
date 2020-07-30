b81a60dd66672efd42046a2eb6ba4fd11b6c2b78
ignatandrei <ignatandrei@yahoo.com>
2020-07-30 09:42:43 +0300

    now generating calsses

---
+++ stankinsv2/solution/GenerateAll/FrontEnd/Angular10.0/src/app/WebAPIClasses/@Name@.ts
@@ -0,0 +1,59 @@
+﻿@model Stankins.Interfaces.IDataToSent
+@{
+
+    var dt= Model.FindAfterName("@Name@").Value;
+    var nrCols =dt.Columns.Count;
+}
+
+
+export class @dt.TableName
+    {
+        
+        
+        public constructor(other:@dt.TableName = null){ 
+
+            if(other != null){
+				this.CopyPropertiesFrom(other, true);
+			}
+                
+        }
+        public CopyPropertiesFrom(other:@dt.TableName, withID: boolean):void{
+            if(withID){
+                this.ID= other.ID;
+            }
+            @for(int iCol = 0;iCol < nrCols; iCol++){
+                var col = dt.Columns[iCol];
+                var colName= col.ColumnName ;
+                
+                <text>
+            this.@colName = other.@colName;
+                </text>
+
+            }
+
+            
+        }
+        public  ID: number;
+            
+        @for(int iCol = 0;iCol < nrCols; iCol++){
+            var col = dt.Columns[iCol];
+            var colName= col.ColumnName ;
+            var colType = col.DataType;
+			var nameType ="";
+			switch(colType.Name.ToLower()){
+				case "string":
+					nameType="string";
+					break;
+				default:
+					nameType="!!!!"+@colType.Name;
+					break;
+			}
+
+            <text>
+            public @colName : @nameType;
+            </text>
+
+        }
+        
+    }
+
--- stankinsv2/solution/GenerateAll/FrontEnd/Angular10.0/src/app/WebAPIComponents/@Name@.component.ts
+++ stankinsv2/solution/GenerateAll/FrontEnd/Angular10.0/src/app/WebAPIComponents/@Name@.component.ts
@@ -1,8 +1,11 @@
 @{
 	var angular="@angular";
 	var Component = "@Component";
+	var dt= Model.FindAfterName("@Name@").Value;
+	
 }
 import { Component, OnInit } from '@(angular)/core';
+import{ @dt.TableName } from './../WebAPIClasses/@dt.TableName';
 
 @(Component)({
   selector: 'app-@Name@-component',
