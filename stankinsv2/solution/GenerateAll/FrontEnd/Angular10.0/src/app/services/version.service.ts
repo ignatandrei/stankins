0234b5c29a6b5dc0450965736d5ac7f6f96f0efa
ignatandrei <ignatandrei@yahoo.com>
2020-07-30 05:12:48 +0300

    save

---
+++ stankinsv2/solution/GenerateAll/FrontEnd/Angular10.0/src/app/services/version.service.ts
@@ -0,0 +1,13 @@
+@{
+  var angular="@angular";
+  var Injectable="@Injectable";
+}
+import { Injectable } from '@angular/core';
+
+@(Injectable)({
+  providedIn: 'root'
+})
+export class VersionService {
+
+  constructor() { }
+}
