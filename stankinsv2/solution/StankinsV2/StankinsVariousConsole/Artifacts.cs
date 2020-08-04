dbb5265b5cdd623285527dde6aa8a278a50304f9
ignatandrei <ignatandrei@yahoo.com>
2020-08-04 07:14:13 +0300

    trying to download artifacts

---
+++ stankinsv2/solution/StankinsV2/StankinsVariousConsole/Artifacts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StankinsVariousConsole
+{
+    internal class Artifacts
+    {
+
+
+        public int total_count { get; set; }
+        public Artifact[] artifacts { get; set; }
+    }
+
+    public class Artifact
+    {
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string name { get; set; }
+        public int size_in_bytes { get; set; }
+        public string url { get; set; }
+        public string archive_download_url { get; set; }
+        public bool expired { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+
+}
--- stankinsv2/solution/StankinsV2/StankinsVariousConsole/Program.cs
+++ stankinsv2/solution/StankinsV2/StankinsVariousConsole/Program.cs
@@ -25,9 +25,209 @@ using YamlDotNet.RepresentationModel;
 using Stankins.Razor;
 using Stankins.Excel;
 using Octokit;
+using System.Net.Http;
 
 namespace StankinsVariousConsole
 {
+    public class ActionRuns
+    {
+        public int total_count { get; set; }
+        public Workflow_Runs[] workflow_runs { get; set; }
+    }
+
+    public class Workflow_Runs
+    {
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string head_branch { get; set; }
+        public string head_sha { get; set; }
+        public int run_number { get; set; }
+        public string _event { get; set; }
+        public string status { get; set; }
+        public string conclusion { get; set; }
+        public int workflow_id { get; set; }
+        public string url { get; set; }
+        public string html_url { get; set; }
+        public object[] pull_requests { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public string jobs_url { get; set; }
+        public string logs_url { get; set; }
+        public string check_suite_url { get; set; }
+        public string artifacts_url { get; set; }
+        public string cancel_url { get; set; }
+        public string rerun_url { get; set; }
+        public string workflow_url { get; set; }
+        public Head_Commit head_commit { get; set; }
+        public Repository repository { get; set; }
+        public Head_Repository head_repository { get; set; }
+    }
+
+    public class Head_Commit
+    {
+        public string id { get; set; }
+        public string tree_id { get; set; }
+        public string message { get; set; }
+        public DateTime timestamp { get; set; }
+        public Author author { get; set; }
+        public Committer committer { get; set; }
+    }
+
+    public class Author
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+
+    public class Committer
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+
+    public class Repository
+    {
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string name { get; set; }
+        public string full_name { get; set; }
+        public bool _private { get; set; }
+        public Owner owner { get; set; }
+        public string html_url { get; set; }
+        public object description { get; set; }
+        public bool fork { get; set; }
+        public string url { get; set; }
+        public string forks_url { get; set; }
+        public string keys_url { get; set; }
+        public string collaborators_url { get; set; }
+        public string teams_url { get; set; }
+        public string hooks_url { get; set; }
+        public string issue_events_url { get; set; }
+        public string events_url { get; set; }
+        public string assignees_url { get; set; }
+        public string branches_url { get; set; }
+        public string tags_url { get; set; }
+        public string blobs_url { get; set; }
+        public string git_tags_url { get; set; }
+        public string git_refs_url { get; set; }
+        public string trees_url { get; set; }
+        public string statuses_url { get; set; }
+        public string languages_url { get; set; }
+        public string stargazers_url { get; set; }
+        public string contributors_url { get; set; }
+        public string subscribers_url { get; set; }
+        public string subscription_url { get; set; }
+        public string commits_url { get; set; }
+        public string git_commits_url { get; set; }
+        public string comments_url { get; set; }
+        public string issue_comment_url { get; set; }
+        public string contents_url { get; set; }
+        public string compare_url { get; set; }
+        public string merges_url { get; set; }
+        public string archive_url { get; set; }
+        public string downloads_url { get; set; }
+        public string issues_url { get; set; }
+        public string pulls_url { get; set; }
+        public string milestones_url { get; set; }
+        public string notifications_url { get; set; }
+        public string labels_url { get; set; }
+        public string releases_url { get; set; }
+        public string deployments_url { get; set; }
+    }
+
+    public class Owner
+    {
+        public string login { get; set; }
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string avatar_url { get; set; }
+        public string gravatar_id { get; set; }
+        public string url { get; set; }
+        public string html_url { get; set; }
+        public string followers_url { get; set; }
+        public string following_url { get; set; }
+        public string gists_url { get; set; }
+        public string starred_url { get; set; }
+        public string subscriptions_url { get; set; }
+        public string organizations_url { get; set; }
+        public string repos_url { get; set; }
+        public string events_url { get; set; }
+        public string received_events_url { get; set; }
+        public string type { get; set; }
+        public bool site_admin { get; set; }
+    }
+
+    public class Head_Repository
+    {
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string name { get; set; }
+        public string full_name { get; set; }
+        public bool _private { get; set; }
+        public Owner1 owner { get; set; }
+        public string html_url { get; set; }
+        public object description { get; set; }
+        public bool fork { get; set; }
+        public string url { get; set; }
+        public string forks_url { get; set; }
+        public string keys_url { get; set; }
+        public string collaborators_url { get; set; }
+        public string teams_url { get; set; }
+        public string hooks_url { get; set; }
+        public string issue_events_url { get; set; }
+        public string events_url { get; set; }
+        public string assignees_url { get; set; }
+        public string branches_url { get; set; }
+        public string tags_url { get; set; }
+        public string blobs_url { get; set; }
+        public string git_tags_url { get; set; }
+        public string git_refs_url { get; set; }
+        public string trees_url { get; set; }
+        public string statuses_url { get; set; }
+        public string languages_url { get; set; }
+        public string stargazers_url { get; set; }
+        public string contributors_url { get; set; }
+        public string subscribers_url { get; set; }
+        public string subscription_url { get; set; }
+        public string commits_url { get; set; }
+        public string git_commits_url { get; set; }
+        public string comments_url { get; set; }
+        public string issue_comment_url { get; set; }
+        public string contents_url { get; set; }
+        public string compare_url { get; set; }
+        public string merges_url { get; set; }
+        public string archive_url { get; set; }
+        public string downloads_url { get; set; }
+        public string issues_url { get; set; }
+        public string pulls_url { get; set; }
+        public string milestones_url { get; set; }
+        public string notifications_url { get; set; }
+        public string labels_url { get; set; }
+        public string releases_url { get; set; }
+        public string deployments_url { get; set; }
+    }
+
+    public class Owner1
+    {
+        public string login { get; set; }
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string avatar_url { get; set; }
+        public string gravatar_id { get; set; }
+        public string url { get; set; }
+        public string html_url { get; set; }
+        public string followers_url { get; set; }
+        public string following_url { get; set; }
+        public string gists_url { get; set; }
+        public string starred_url { get; set; }
+        public string subscriptions_url { get; set; }
+        public string organizations_url { get; set; }
+        public string repos_url { get; set; }
+        public string events_url { get; set; }
+        public string received_events_url { get; set; }
+        public string type { get; set; }
+        public bool site_admin { get; set; }
+    }
     public class StankinsGenerator
     {
         public Backend[] backend { get; set; }
@@ -104,7 +304,7 @@ namespace StankinsVariousConsole
 
             var backendFolderName = @"NETCore3.1";
             var frontendFolderName = @"Angular10.0";
-            frontendFolderName = @"Angular10.0Full";
+            //frontendFolderName = @"Angular10.0Full";
             var backend = stData.backend.First(it => it.folder == backendFolderName);
             var frontEnd= stData.frontend.First(it => it.folder == frontendFolderName);
             //wt  new-tab -d C:\test\backend\NETCore3.1\TestWebAPI ; split-pane -d C:\test\frontend\Angular10.0
@@ -222,11 +422,62 @@ namespace StankinsVariousConsole
             var save = new SenderOutputToFolder(outputFolder, false, "OutputString");
             data = await save.TransformData(data);
 
-            //var b = await CreateBranch(g);
-            //var final = await CommitDir(b, outputFolder);
+            Console.WriteLine($"branch : {g} ");
+            var b = await CreateBranch(g);
+            var final = await CommitDir(b, outputFolder);
+
+            var ret = await waitForRuns(g);
+            Console.WriteLine(string.Join(Environment.NewLine, ret));
             return true;
         }
-        static string CredentialsToken = "please generate";
+
+        static async Task<string[]> waitForRuns(string g)
+        {
+            var c = new HttpClient();
+            c.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0");
+            Workflow_Runs[] runs = null;
+            var nrWaits = 20;
+            while (--nrWaits > 0)
+            {
+                Console.WriteLine($"wait {nrWaits}");
+
+                var str = await c.GetStringAsync("https://api.github.com/repos/ignatandrei/generateApp/actions/runs");
+                var act = System.Text.Json.JsonSerializer.Deserialize<ActionRuns>(str);
+                runs = act.workflow_runs.Where(it => it.head_branch == g).ToArray();
+                var allStatus = runs.Select(it => it.status).Distinct().ToArray();
+                if (runs.Length ==3 && allStatus.Length == 1 && allStatus[0] == "completed")
+                    break;
+
+                Console.WriteLine($"wait {nrWaits}");   
+                await Task.Delay(1000 * 60);
+
+            }
+
+
+            var lastRun = runs.FirstOrDefault();//search for "commit test...
+            if (lastRun == null)
+                throw new ArgumentException($"cannot find runs for {g}");
+
+            if (lastRun.conclusion != "success")
+            {
+                throw new ArgumentException($"the run for {g} is without success. Please contact us to find more");
+            }
+            return new string[] { lastRun.html_url };
+            //var arr = lastRun.check_suite_url.Split("/");
+            //string suite = arr.Last();
+
+
+
+            //var arts = await c.GetStringAsync(lastRun.artifacts_url);
+            //var artifacts = System.Text.Json.JsonSerializer.Deserialize<Artifacts>(arts);
+            //return artifacts.artifacts.Select(it =>
+
+            //     $"https://github.com/ignatandrei/generateApp/suites/{suite}/artifacts/{it.id}"
+            // ).ToArray();
+
+
+        }
+        static string CredentialsToken = "please get credentials";
         static async Task<bool> CommitDir(string headMasterRef, string folder)
         {
             var sep = Path.DirectorySeparatorChar;
@@ -246,7 +497,7 @@ namespace StankinsVariousConsole
             var owner = "ignatandrei";
             var repo = "generateApp";
             var repoId = (await Client.Repository.Get(owner, repo)).Id;
-
+            //Client.Repository.Release.Get(repoId,"asd").Result.Assets.First().
             //var headMasterRef = "heads/master";
             //headMasterRef = "refs/heads/tsf1";
             var masterReference = await Client.Git.Reference.Get(repoId, headMasterRef); // Get reference of master branch
